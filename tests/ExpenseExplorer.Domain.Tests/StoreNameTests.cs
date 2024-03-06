f72c9bfdc59b943c08116b10c933b33fb634f263
frognar <sebastian.przyszlak@gmail.com>
2024-03-06 06:49:06 +0100

    Remove redundant test

--- tests/ExpenseExplorer.Domain.Tests/StoreNameTests.cs
+++ tests/ExpenseExplorer.Domain.Tests/StoreNameTests.cs
@@ -3,12 +3,6 @@ using ExpenseExplorer.Domain.ValueObjects;
 namespace ExpenseExplorer.Domain.Tests;
 
 public class StoreTests {
-  [Property(Arbitrary = [typeof(NonEmptyStringGenerator)])]
-  public void StoreNameIsSetDuringConstruction(string name) {
-    Store store = Store.Create(name);
-    store.Name.Should().Be(name.Trim());
-  }
-
   [Property(Arbitrary = [typeof(EmptyStringGenerator)])]
   public void ThrowsExceptionWhenNameIsEmpty(string name) {
     Action act = () => _ = Store.Create(name);
