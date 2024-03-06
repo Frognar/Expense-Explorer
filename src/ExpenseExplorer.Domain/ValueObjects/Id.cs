984119f1bb3521948b4104df6f89dede1119be79
frognar <sebastian.przyszlak@gmail.com>
2024-03-06 06:17:12 +0100

    Add factory method to generate unique ids

--- src/ExpenseExplorer.Domain/ValueObjects/Id.cs
+++ src/ExpenseExplorer.Domain/ValueObjects/Id.cs
@@ -1,9 +1,17 @@
 namespace ExpenseExplorer.Domain.ValueObjects;
 
 public readonly record struct Id {
+  public Id() {
+    Value = Guid.NewGuid().ToString("N");
+  }
+
   public Id(string value) {
     Value = value.Trim();
   }
 
   public string Value { get; }
+
+  public static Id Unique() {
+    return new Id();
+  }
 }
--- tests/ExpenseExplorer.Domain.Tests/IdTests.cs
+++ tests/ExpenseExplorer.Domain.Tests/IdTests.cs
@@ -12,4 +12,15 @@ public class IdTests {
     Assert.NotEqual(new Id("123"), new Id("234"));
     Assert.NotEqual(new Id("123"), new Id("234"));
   }
+
+  [Fact]
+  public void IdsAreUnique() {
+    List<Id> ids = GenerateIds(1000);
+    HashSet<Id> idSet = ids.ToHashSet();
+    Assert.Equal(idSet.Count, ids.Count);
+  }
+
+  private static List<Id> GenerateIds(int n) {
+    return Enumerable.Range(0, n).Select(_ => Id.Unique()).ToList();
+  }
 }
