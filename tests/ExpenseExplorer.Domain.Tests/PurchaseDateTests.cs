5c15d98e52d748c07db9cfbc7bcb3a1d75549795
frognar <sebastian.przyszlak@gmail.com>
2024-03-06 06:59:07 +0100

    Refactor PurchaseDate tests

--- tests/ExpenseExplorer.Domain.Tests/PurchaseDateTests.cs
+++ tests/ExpenseExplorer.Domain.Tests/PurchaseDateTests.cs
@@ -5,13 +5,17 @@ namespace ExpenseExplorer.Domain.Tests;
 public class PurchaseDateTests {
   [Property(Arbitrary = [typeof(NonFutureDateOnlyGenerator)])]
   public void SetsDate(DateOnly date) {
-    PurchaseDate purchaseDate = PurchaseDate.Create(date, todayDateOnly);
+    PurchaseDate purchaseDate = CreatePurchaseDate(date);
     purchaseDate.Date.Should().Be(date);
   }
 
   [Property(Arbitrary = [typeof(FutureDateOnlyGenerator)])]
   public void ThrowsExceptionWhenDateIsInTheFuture(DateOnly date) {
-    Action act = () => _ = PurchaseDate.Create(date, todayDateOnly);
+    Action act = () => _ = CreatePurchaseDate(date);
     act.Should().Throw<ArgumentException>();
+  }
+
+  private static PurchaseDate CreatePurchaseDate(DateOnly date) {
+    return PurchaseDate.Create(date, todayDateOnly);
   }
 }
