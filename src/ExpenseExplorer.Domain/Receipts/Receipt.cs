7908d11ee56517529f568ae21667475279c9756a
frognar <sebastian.przyszlak@gmail.com>
2024-03-08 06:16:59 +0100

    Create ReceiptCreated event when receipt is created

--- src/ExpenseExplorer.Domain/Receipts/Receipt.cs
+++ src/ExpenseExplorer.Domain/Receipts/Receipt.cs
@@ -1,19 +1,31 @@
 namespace ExpenseExplorer.Domain.Receipts;
 
+using ExpenseExplorer.Domain.Receipts.Events;
 using ExpenseExplorer.Domain.ValueObjects;
 
 public class Receipt
 {
-  public Receipt(Id id, Store store, PurchaseDate purchaseDate)
+  private Receipt(Id id, Store store, PurchaseDate purchaseDate)
   {
-    Id = id;
-    Store = store;
-    PurchaseDate = purchaseDate;
+    ReceiptCreated fact = new(id, store, purchaseDate);
+    Apply(fact);
   }
 
-  public Id Id { get; }
+  public Id Id { get; private set; } = null!;
 
-  public Store Store { get; }
+  public Store Store { get; private set; } = null!;
 
-  public PurchaseDate PurchaseDate { get; }
+  public PurchaseDate PurchaseDate { get; private set; } = null!;
+
+  public static Receipt New(Store store, PurchaseDate purchaseDate)
+  {
+    return new Receipt(Id.Unique(), store, purchaseDate);
+  }
+
+  private void Apply(ReceiptCreated e)
+  {
+    Id = e.Id;
+    Store = e.Store;
+    PurchaseDate = e.PurchaseDate;
+  }
 }
--- tests/ExpenseExplorer.Domain.Tests/ReceiptTests.cs
+++ tests/ExpenseExplorer.Domain.Tests/ReceiptTests.cs
@@ -8,7 +8,10 @@ public class ReceiptTests
   [Property(Arbitrary = [typeof(NonFutureDateOnlyGenerator), typeof(NonEmptyStringGenerator)])]
   public void CanBeCreated(DateOnly purchaseDate, string store)
   {
-    Receipt receipt = new(Id.Unique(), Store.Create(store), PurchaseDate.Create(purchaseDate, todayDateOnly));
+    Receipt receipt = Receipt.New(Store.Create(store), PurchaseDate.Create(purchaseDate, todayDateOnly));
     receipt.Should().NotBeNull();
+    receipt.Id.Should().NotBeNull();
+    receipt.Store.Should().Be(Store.Create(store));
+    receipt.PurchaseDate.Should().Be(PurchaseDate.Create(purchaseDate, todayDateOnly));
   }
 }
