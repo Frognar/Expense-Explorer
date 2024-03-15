a4c0f3c8a086f697fae5b69bb901432d78e3e41e
frognar <sebastian.przyszlak@gmail.com>
2024-03-15 17:31:11 +0100

    Can create Either left

---
+++ src/ExpenseExplorer.Application/Monads/Either.cs
@@ -0,0 +1,12 @@
+﻿namespace ExpenseExplorer.Application.Monads;
+
+public class Either<L, R>
+{
+  private Either(L left, R right, bool isLeft)
+  {
+  }
+
+#pragma warning disable CA1000
+  public static Either<L, R> Left(L left) => new(left, default!, true);
+#pragma warning restore CA1000
+}
---
+++ tests/ExpenseExplorer.Application.Tests/EitherTests.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseExplorer.Application.Tests;
+
+using ExpenseExplorer.Application.Monads;
+
+public class EitherTests
+{
+  [Property]
+  public void CanCreateLeft(int left)
+  {
+    var either = Either<int, string>.Left(left);
+    either.Should().NotBeNull();
+  }
+}
