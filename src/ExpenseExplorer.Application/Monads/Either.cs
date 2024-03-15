b2717559c63a2ed327b8d35b51de974a84ae8e03
frognar <sebastian.przyszlak@gmail.com>
2024-03-15 17:46:41 +0100

    Refactor Either

--- src/ExpenseExplorer.Application/Monads/Either.cs
+++ src/ExpenseExplorer.Application/Monads/Either.cs
@@ -1,28 +1,47 @@
 ﻿namespace ExpenseExplorer.Application.Monads;
 
-public class Either<L, R>
+public sealed class Either<L, R>
 {
-  private readonly L left;
-  private readonly R right;
-  private readonly bool isLeft;
+  private readonly IEither either;
 
-  private Either(L left, R right, bool isLeft)
+  private Either(IEither either)
   {
-    this.left = left;
-    this.right = right;
-    this.isLeft = isLeft;
+    this.either = either;
+  }
+
+  private interface IEither
+  {
+    T Match<T>(Func<L, T> onLeft, Func<R, T> onRight);
   }
 
 #pragma warning disable CA1000
-  public static Either<L, R> Left(L left) => new(left, default!, true);
+  public static Either<L, R> Left(L left) => new(new EitherLeft(left));
 
-  public static Either<L, R> Right(R right) => new(default!, right, false);
+  public static Either<L, R> Right(R right) => new(new EitherRight(right));
 #pragma warning restore CA1000
 
   public T Match<T>(Func<L, T> onLeft, Func<R, T> onRight)
   {
     ArgumentNullException.ThrowIfNull(onLeft);
     ArgumentNullException.ThrowIfNull(onRight);
-    return isLeft ? onLeft(left) : onRight(right);
+    return either.Match(onLeft, onRight);
+  }
+
+  private sealed class EitherLeft : IEither
+  {
+    private readonly L left;
+
+    public EitherLeft(L left) => this.left = left;
+
+    public T Match<T>(Func<L, T> onLeft, Func<R, T> onRight) => onLeft(left);
+  }
+
+  private sealed class EitherRight : IEither
+  {
+    private readonly R right;
+
+    public EitherRight(R right) => this.right = right;
+
+    public T Match<T>(Func<L, T> onLeft, Func<R, T> onRight) => onRight(right);
   }
 }
