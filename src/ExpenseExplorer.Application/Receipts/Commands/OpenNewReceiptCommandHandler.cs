863d8cd3608d59e85d6bfe5ee485d6a7789bb041
frognar <sebastian.przyszlak@gmail.com>
2024-03-17 17:33:03 +0100

    Save receipt when valid

--- src/ExpenseExplorer.Application/Receipts/Commands/OpenNewReceiptCommandHandler.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/OpenNewReceiptCommandHandler.cs
@@ -1,25 +1,26 @@
 namespace ExpenseExplorer.Application.Receipts.Commands;
 
+using System.Diagnostics;
 using ExpenseExplorer.Application.Errors;
 using ExpenseExplorer.Application.Monads;
 using ExpenseExplorer.Application.Receipts.Persistence;
 using ExpenseExplorer.Application.Validations;
 using ExpenseExplorer.Domain.Receipts;
 
-public class OpenNewReceiptCommandHandler
+public class OpenNewReceiptCommandHandler(IReceiptRepository repository)
 {
-  public OpenNewReceiptCommandHandler(IReceiptRepository repository)
-  {
-  }
+  private readonly IReceiptRepository repository = repository;
 
-#pragma warning disable CA1822
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
   public async Task<Either<Failure, Receipt>> HandleAsync(OpenNewReceiptCommand command)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-#pragma warning restore CA1822
   {
     ArgumentNullException.ThrowIfNull(command);
     Validated<Receipt> receipt = ReceiptValidator.Validate(command);
+    if (receipt.IsValid)
+    {
+      Receipt r = receipt.Match(_ => throw new UnreachableException(), r => r);
+      await repository.Save(r);
+    }
+
     return receipt.ToEither().MapLeft(f => (Failure)f);
   }
 }
--- tests/ExpenseExplorer.Application.Tests/OpenNewReceiptHandlerTests.cs
+++ tests/ExpenseExplorer.Application.Tests/OpenNewReceiptHandlerTests.cs
@@ -1,15 +1,17 @@
 namespace ExpenseExplorer.Application.Tests;
 
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using ExpenseExplorer.Application.Errors;
 using ExpenseExplorer.Application.Monads;
 using ExpenseExplorer.Application.Receipts.Commands;
 using ExpenseExplorer.Application.Receipts.Persistence;
 using ExpenseExplorer.Domain.Receipts;
-using ExpenseExplorer.Infrastructure.Receipts.Persistence;
 
 public class OpenNewReceiptHandlerTests
 {
+  private readonly FakeReceiptRepository repository = new();
+
   [Property(Arbitrary = [typeof(ValidOpenNewReceiptCommandGenerator)])]
   public async Task CanHandleValidCommand(OpenNewReceiptCommand command)
   {
@@ -29,10 +31,27 @@ public class OpenNewReceiptHandlerTests
     failure.Should().BeOfType<ValidationFailure>();
   }
 
-  private static async Task<Either<Failure, Receipt>> Handle(OpenNewReceiptCommand command)
+  [Property(Arbitrary = [typeof(ValidOpenNewReceiptCommandGenerator)])]
+  public async Task SavesReceiptWhenValidCommand(OpenNewReceiptCommand command)
   {
-    IReceiptRepository repository = new InMemoryReceiptRepository();
+    var receipt = (await Handle(command))
+      .Match(_ => throw new UnreachableException(), receipt => receipt);
+
+    repository.Should().Contain(receipt);
+  }
+
+  private async Task<Either<Failure, Receipt>> Handle(OpenNewReceiptCommand command)
+  {
     OpenNewReceiptCommandHandler handler = new(repository);
     return await handler.HandleAsync(command);
+  }
+
+  private sealed class FakeReceiptRepository : Collection<Receipt>, IReceiptRepository
+  {
+    public Task Save(Receipt receipt)
+    {
+      Add(receipt);
+      return Task.CompletedTask;
+    }
   }
 }
