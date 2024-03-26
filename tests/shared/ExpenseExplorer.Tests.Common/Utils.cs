0b0ec6884f98f8b8201684a0976d4554a51b09c4
frognar <sebastian.przyszlak@gmail.com>
2024-03-26 05:49:39 +0100

    Change Today and TodayDateOnly in tests from readonly fields to get only properties

--- tests/shared/ExpenseExplorer.Tests.Common/Utils.cs
+++ tests/shared/ExpenseExplorer.Tests.Common/Utils.cs
@@ -2,6 +2,7 @@ namespace ExpenseExplorer.Tests.Common;
 
 public static class Utils
 {
-  public static readonly DateTime Today = new(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-  public static readonly DateOnly TodayDateOnly = new(Today.Year, Today.Month, Today.Day);
+  public static DateTime Today { get; } = new(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+  public static DateOnly TodayDateOnly { get; } = new(Today.Year, Today.Month, Today.Day);
 }
