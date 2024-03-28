29ad56710056791b251203e18b5baa72b3596c40
frognar <sebastian.przyszlak@gmail.com>
2024-03-28 07:34:26 +0100

    Add DI abstraction package to Application layer

--- src/ExpenseExplorer.Application/Commands/ICommand.cs
+++ src/ExpenseExplorer.Application/Commands/ICommand.cs
@@ -8,6 +8,6 @@ using System.Diagnostics.CodeAnalysis;
   Justification = "Marker interface for command pattern.")]
 [SuppressMessage(
   "Major Code Smell",
-  "S2326: Unused type parameters should be removed",
+  "S2326:Unused type parameters should be removed",
   Justification = "Marker interface for command pattern.")]
 public interface ICommand<TResponse>;
