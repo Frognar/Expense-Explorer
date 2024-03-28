0dad32fd9ce0db594b9fc73e68183dc1af0ef142
frognar <sebastian.przyszlak@gmail.com>
2024-03-28 06:58:59 +0100

    Add ICommand interface implementation to existing commands

--- src/ExpenseExplorer.Application/Receipts/Commands/OpenNewReceiptCommand.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/OpenNewReceiptCommand.cs
@@ -1,3 +1,9 @@
 namespace ExpenseExplorer.Application.Receipts.Commands;
 
-public record OpenNewReceiptCommand(string StoreName, DateOnly PurchaseDate, DateOnly Today);
+using ExpenseExplorer.Application.Commands;
+using ExpenseExplorer.Application.Errors;
+using ExpenseExplorer.Application.Monads;
+using ExpenseExplorer.Domain.Receipts;
+
+public record OpenNewReceiptCommand(string StoreName, DateOnly PurchaseDate, DateOnly Today)
+  : ICommand<Either<Failure, Receipt>>;
