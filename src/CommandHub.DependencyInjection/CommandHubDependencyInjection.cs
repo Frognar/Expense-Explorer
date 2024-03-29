0e09d302a5e52ba84aaa8162f27f7d09a71b06d5
frognar <sebastian.przyszlak@gmail.com>
2024-03-29 06:33:08 +0100

    Move dependency injection logic for command handling to new project CommandHub.DependencyInjection

---
+++ src/CommandHub.DependencyInjection/CommandHubDependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿namespace CommandHub.DependencyInjection;
+
+using System.Reflection;
+using CommandHub.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+public static class CommandHubDependencyInjection
+{
+  public static void AddCommandHub(this IServiceCollection services, params Assembly[] assemblies)
+  {
+    services.RegisterHandlers(assemblies);
+    services.AddScoped<ISender>(sp => CommandRegistry.CreateSender(sp.GetRequiredService, assemblies));
+  }
+
+  private static void RegisterHandlers(this IServiceCollection services, Assembly[] assemblies)
+  {
+    var descriptors = CommandRegistry.GetHandlerTypes(assemblies)
+      .Select(t => new ServiceDescriptor(t.Interface, t.Implementation, ServiceLifetime.Scoped));
+
+    services.TryAdd(descriptors);
+  }
+}
--- src/ExpenseExplorer.Application/ApplicationDependencyInjection.cs
+++ src/ExpenseExplorer.Application/ApplicationDependencyInjection.cs
@@ -1,24 +1,12 @@
 namespace ExpenseExplorer.Application;
 
-using System.Reflection;
-using CommandHub.Commands;
+using CommandHub.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 
 public static class ApplicationDependencyInjection
 {
   public static void AddApplication(this IServiceCollection services)
   {
-    Assembly assembly = typeof(ApplicationDependencyInjection).Assembly;
-    services.RegisterHandlers(assembly);
-    services.AddScoped<ISender>(sp => CommandRegistry.CreateSender(sp.GetRequiredService, assembly));
-  }
-
-  private static void RegisterHandlers(this IServiceCollection services, Assembly assembly)
-  {
-    var descriptors = CommandRegistry.GetHandlerTypes(assembly)
-      .Select(t => new ServiceDescriptor(t.Interface, t.Implementation, ServiceLifetime.Scoped));
-
-    services.TryAdd(descriptors);
+    services.AddCommandHub(typeof(ApplicationDependencyInjection).Assembly);
   }
 }
