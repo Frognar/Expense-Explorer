be80e55ec441b85a34e46d110f4bfec807b20168
frognar <sebastian.przyszlak@gmail.com>
2024-03-29 06:22:29 +0100

    Refactor ApplicationDependencyInjection by creating CommandRegistry

--- src/ExpenseExplorer.Application/ApplicationDependencyInjection.cs
+++ src/ExpenseExplorer.Application/ApplicationDependencyInjection.cs
@@ -1,7 +1,7 @@
 namespace ExpenseExplorer.Application;
 
+using System.Reflection;
 using ExpenseExplorer.Application.Commands;
-using ExpenseExplorer.Application.Commands.Wrappers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -9,49 +9,16 @@ public static class ApplicationDependencyInjection
 {
   public static void AddApplication(this IServiceCollection services)
   {
-    services.RegisterCommandHandlers();
-    Dictionary<Type, BaseCommandHandlerWrapper> wrappers = CreateCommandHandlerWrappers();
-    services.AddScoped<ISender>(sp => new Sender(sp.GetRequiredService, wrappers));
+    Assembly assembly = typeof(ApplicationDependencyInjection).Assembly;
+    services.RegisterHandlers(assembly);
+    services.AddScoped<ISender>(sp => CommandRegistry.CreateSender(sp.GetRequiredService, assembly));
   }
 
-  private static void RegisterCommandHandlers(this IServiceCollection services)
+  private static void RegisterHandlers(this IServiceCollection services, Assembly assembly)
   {
-    IEnumerable<ServiceDescriptor> serviceDescriptors = GetClassesImplementingInterface(typeof(ICommandHandler<,>))
-      .Select(type => new { type, inter = type.GetInterface("ICommandHandler`2")! })
-      .Select(x => new ServiceDescriptor(x.inter, x.type, ServiceLifetime.Scoped));
+    var descriptors = CommandRegistry.GetHandlerTypes(assembly)
+      .Select(t => new ServiceDescriptor(t.Interface, t.Implementation, ServiceLifetime.Scoped));
 
-    services.TryAdd(serviceDescriptors);
-  }
-
-  private static IEnumerable<Type> GetClassesImplementingInterface(Type interfaceType)
-  {
-    return typeof(ApplicationDependencyInjection).Assembly.ExportedTypes
-      .Where(
-        type =>
-        {
-          IEnumerable<Type> genericInterfaceTypes = type.GetInterfaces().Where(i => i.IsGenericType);
-          bool implementRequestTypes = genericInterfaceTypes
-            .Any(i => i.GetGenericTypeDefinition() == interfaceType);
-
-          return type is { IsInterface: false, IsAbstract: false } && implementRequestTypes;
-        });
-  }
-
-  private static Dictionary<Type, BaseCommandHandlerWrapper> CreateCommandHandlerWrappers()
-  {
-    return GetClassesImplementingInterface(typeof(ICommand<>))
-      .ToDictionary(command => command, CreateWrapper);
-  }
-
-  private static BaseCommandHandlerWrapper CreateWrapper(Type commandType)
-  {
-    Type wrapperType = typeof(CommandHandlerWrapperImpl<,>).MakeGenericType(
-      commandType,
-      commandType.GetInterface("ICommand`1")!.GetGenericArguments()[0]);
-
-    object wrapper = Activator.CreateInstance(wrapperType)
-                     ?? throw new InvalidOperationException($"Failed to create instance of {wrapperType}.");
-
-    return (BaseCommandHandlerWrapper)wrapper;
+    services.TryAdd(descriptors);
   }
 }
---
+++ src/ExpenseExplorer.Application/Commands/CommandRegistry.cs
@@ -0,0 +1,63 @@
+namespace ExpenseExplorer.Application.Commands;
+
+using System.Reflection;
+using ExpenseExplorer.Application.Commands.Wrappers;
+
+public static class CommandRegistry
+{
+  public static ISender CreateSender(Func<Type, object> serviceProvider, params Assembly[] assemblies)
+  {
+    return new Sender(serviceProvider, CreateWrappers(assemblies));
+  }
+
+  public static IEnumerable<(Type Interface, Type Implementation)> GetHandlerTypes(params Assembly[] assemblies)
+  {
+    return GetClassesImplementing(typeof(ICommandHandler<,>), assemblies)
+      .Select(type => (type.GetInterface(typeof(ICommandHandler<,>).Name)!, type));
+  }
+
+  private static Dictionary<Type, BaseCommandHandlerWrapper> CreateWrappers(IEnumerable<Assembly> assemblies)
+  {
+    return GetClassesImplementing(typeof(ICommand<>), assemblies)
+      .ToDictionary(command => command, CreateWrapper);
+  }
+
+  private static BaseCommandHandlerWrapper CreateWrapper(Type commandType)
+  {
+    Type wrapperType = typeof(CommandHandlerWrapperImpl<,>).MakeGenericType(
+      commandType,
+      commandType.GetInterface(typeof(ICommand<>).Name)!.GetGenericArguments()[0]);
+
+    object wrapper = Activator.CreateInstance(wrapperType)
+                     ?? throw new InvalidOperationException($"Failed to create instance of {wrapperType}.");
+
+    return (BaseCommandHandlerWrapper)wrapper;
+  }
+
+  private static IEnumerable<Type> GetClassesImplementing(Type interfaceType, IEnumerable<Assembly> assemblies)
+  {
+    return assemblies.SelectMany(GetClassesImplementing(interfaceType));
+  }
+
+  private static Func<Assembly, IEnumerable<Type>> GetClassesImplementing(Type interfaceType)
+  {
+    return assembly => GetClassesImplementing(interfaceType, assembly);
+  }
+
+  private static IEnumerable<Type> GetClassesImplementing(Type interfaceType, Assembly assembly)
+  {
+    return assembly.ExportedTypes.Where(IsImplementing(interfaceType));
+  }
+
+  private static Func<Type, bool> IsImplementing(Type interfaceType)
+  {
+    return classType => IsImplementing(interfaceType, classType);
+  }
+
+  private static bool IsImplementing(Type interfaceType, Type type)
+  {
+    IEnumerable<Type> genericInterfaceTypes = type.GetInterfaces().Where(i => i.IsGenericType);
+    bool implementRequestTypes = genericInterfaceTypes.Any(i => i.GetGenericTypeDefinition() == interfaceType);
+    return type is { IsInterface: false, IsAbstract: false } && implementRequestTypes;
+  }
+}
