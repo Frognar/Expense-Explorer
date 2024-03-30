811767ed99d569f0bdb0e8664745bbdb9be31595
frognar <sebastian.przyszlak@gmail.com>
2024-03-30 08:46:39 +0100

    Separate integration tests

---
+++ tests/ExpenseExplorer.API.Tests.Integration/Usings.cs
@@ -0,0 +1,7 @@
+global using ExpenseExplorer.Tests.Common;
+global using ExpenseExplorer.Tests.Common.Generators;
+global using FluentAssertions;
+global using FsCheck;
+global using FsCheck.Fluent;
+global using FsCheck.Xunit;
+global using static ExpenseExplorer.Tests.Common.Utils;
