307d20d0f6276aa6de2e31ac02764ae4ff5f195b
frognar <sebastian.przyszlak@gmail.com>
2024-03-31 09:09:29 +0200

    Delete api tests with in-memory repository (replaced by integration tests with actual database)

--- tests/ExpenseExplorer.API.Tests/Receipts/AddPurchaseTests.cs
+++ tests/ExpenseExplorer.API.Tests/Receipts/AddPurchaseTests.cs
@@ -1,41 +1,10 @@
 namespace ExpenseExplorer.API.Tests.Receipts;
 
-using System.Net;
-using System.Net.Http.Json;
 using ExpenseExplorer.API.Contract;
-using Microsoft.AspNetCore.Mvc.Testing;
 
 public class AddPurchaseTests
 {
-  public static IEnumerable<object[]> ValidAddPurchaseRequestData
-  {
-    get
-    {
-      return new List<object[]>
-      {
-        new object[] { new AddPurchaseRequest("item", "category", 1, 0, 0, null), },
-        new object[] { new AddPurchaseRequest("item", "category", 1, 1, 1, null) },
-        new object[] { new AddPurchaseRequest("item", "category", 1, 1, null, "description") },
-      };
-    }
-  }
-
-  public static IEnumerable<object[]> InvalidAddPurchaseRequestData
-  {
-    get
-    {
-      return new List<object[]>
-      {
-        new object[] { new AddPurchaseRequest(string.Empty, "category", 1, 0, null, null) },
-        new object[] { new AddPurchaseRequest("item", string.Empty, 1, 0, null, null) },
-        new object[] { new AddPurchaseRequest("item", "category", 0, 0, null, null) },
-        new object[] { new AddPurchaseRequest("item", "category", 1, -1, null, null) },
-        new object[] { new AddPurchaseRequest("item", "category", 1, 0, -1, null) },
-      };
-    }
-  }
-
-  [Property(Arbitrary = [typeof(NonEmptyStringGenerator), typeof(DateOnlyGenerator), typeof(PositiveDecimalGenerator)])]
+  [Property(Arbitrary = [typeof(NonEmptyStringGenerator), typeof(PositiveDecimalGenerator)])]
   public void ContainsDataGivenDuringConstruction(
     string productName,
     string productCategory,
@@ -58,53 +27,5 @@ public class AddPurchaseTests
     request.UnitPrice.Should().Be(unitPrice);
     request.TotalDiscount.Should().Be(totalDiscount);
     request.Description.Should().Be(description);
-  }
-
-  [Theory]
-  [MemberData(nameof(ValidAddPurchaseRequestData))]
-  public async Task CanAddPurchaseToReceipt(AddPurchaseRequest request)
-  {
-    HttpResponseMessage response = await SendWithValidReceiptId(request);
-    response.StatusCode.ShouldBeIn200Group();
-  }
-
-  [Theory]
-  [MemberData(nameof(ValidAddPurchaseRequestData))]
-  public async Task ContainsAddedPurchaseInResponse(AddPurchaseRequest request)
-  {
-    HttpResponseMessage response = await SendWithValidReceiptId(request);
-    OpenNewReceiptResponse receipt = (await response.Content.ReadFromJsonAsync<OpenNewReceiptResponse>())!;
-    receipt.Purchases.Count().Should().Be(1);
-  }
-
-  [Theory]
-  [MemberData(nameof(ValidAddPurchaseRequestData))]
-  public async Task IsNotFoundWhenReceiptIdIsInvalid(AddPurchaseRequest request)
-  {
-    HttpResponseMessage response = await Send("/api/receipts/invalid-id", request);
-    response.StatusCode.Should().Be(HttpStatusCode.NotFound);
-  }
-
-  [Theory]
-  [MemberData(nameof(InvalidAddPurchaseRequestData))]
-  public async Task IsBadRequestWhenRequestIsInvalid(AddPurchaseRequest request)
-  {
-    HttpResponseMessage response = await SendWithValidReceiptId(request);
-    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-  }
-
-  private static async Task<HttpResponseMessage> SendWithValidReceiptId(AddPurchaseRequest request)
-  {
-    OpenNewReceiptRequest openReceiptRequest = new("Store", TodayDateOnly);
-    HttpResponseMessage response = await Send("/api/receipts", openReceiptRequest);
-    OpenNewReceiptResponse receipt = (await response.Content.ReadFromJsonAsync<OpenNewReceiptResponse>())!;
-    return await Send($"/api/receipts/{receipt.Id}", request);
-  }
-
-  private static async Task<HttpResponseMessage> Send(string uri, object request)
-  {
-    using WebApplicationFactory<Program> webAppFactory = new TestWebApplicationFactory();
-    HttpClient client = webAppFactory.CreateClient();
-    return await client.PostAsJsonAsync(uri, request);
   }
 }
--- tests/ExpenseExplorer.API.Tests/Receipts/OpenNewReceiptTests.cs
+++ tests/ExpenseExplorer.API.Tests/Receipts/OpenNewReceiptTests.cs
@@ -1,46 +1,15 @@
 namespace ExpenseExplorer.API.Tests.Receipts;
 
-using System.Net;
-using System.Net.Http.Json;
 using ExpenseExplorer.API.Contract;
-using Microsoft.AspNetCore.Mvc.Testing;
 
 public class OpenNewReceiptTests
 {
-  public static IEnumerable<object[]> InvalidOpenNewRequestData
+  [Property(Arbitrary = [typeof(NonEmptyStringGenerator), typeof(DateOnlyGenerator)])]
+  public void ContainsDataGivenDuringConstruction(string storeName, DateOnly purchaseDate)
   {
-    get
-    {
-      return new List<object[]>
-      {
-        new object[] { new OpenNewReceiptRequest(string.Empty, TodayDateOnly) },
-        new object[] { new OpenNewReceiptRequest("store", TodayDateOnly.AddDays(1)) },
-      };
-    }
-  }
+    OpenNewReceiptRequest request = new(storeName, purchaseDate);
 
-  [Fact]
-  public async Task CanAddReceipt()
-  {
-    OpenNewReceiptRequest request = new("store", TodayDateOnly);
-
-    HttpResponseMessage response = await Send(request);
-
-    response.StatusCode.ShouldBeIn200Group();
-  }
-
-  [Theory]
-  [MemberData(nameof(InvalidOpenNewRequestData))]
-  public async Task IsBadRequestWhenInvalidRequest(OpenNewReceiptRequest request)
-  {
-    HttpResponseMessage response = await Send(request);
-    response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-  }
-
-  private static async Task<HttpResponseMessage> Send(OpenNewReceiptRequest request)
-  {
-    using WebApplicationFactory<Program> webAppFactory = new TestWebApplicationFactory();
-    HttpClient client = webAppFactory.CreateClient();
-    return await client.PostAsJsonAsync("/api/receipts", request);
+    request.StoreName.Should().Be(storeName);
+    request.PurchaseDate.Should().Be(purchaseDate);
   }
 }
