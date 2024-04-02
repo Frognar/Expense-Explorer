9dcb00b2700c61830771135f61d3adad688d9773
frognar <sebastian.przyszlak@gmail.com>
2024-04-02 06:25:04 +0200

    Add Query Handling method to ISender

--- src/CommandHub/ISender.cs
+++ src/CommandHub/ISender.cs
@@ -1,8 +1,11 @@
 namespace CommandHub;
 
 using CommandHub.Commands;
+using CommandHub.Queries;
 
 public interface ISender
 {
   Task<TResponse> SendAsync<TResponse>(ICommand<TResponse> command, CancellationToken cancellationToken = default);
+
+  Task<TResponse> SendAsync<TResponse>(IQuery<TResponse> query, CancellationToken cancellationToken = default);
 }
--- src/CommandHub/Sender.cs
+++ src/CommandHub/Sender.cs
@@ -1,14 +1,15 @@
 namespace CommandHub;
 
 using CommandHub.Commands;
+using CommandHub.Queries;
 using CommandHub.Wrappers;
 
 internal sealed class Sender(
   Func<Type, object> serviceProvider,
-  IDictionary<Type, BaseHandlerWrapper> commandHandlerWrappers)
+  IDictionary<Type, BaseHandlerWrapper> handlerWrappers)
   : ISender
 {
-  private readonly IDictionary<Type, BaseHandlerWrapper> _commandHandlerWrappers = commandHandlerWrappers;
+  private readonly IDictionary<Type, BaseHandlerWrapper> _handlerWrappers = handlerWrappers;
   private readonly Func<Type, object> _serviceProvider = serviceProvider;
 
   public Task<TResponse> SendAsync<TResponse>(
@@ -16,13 +17,28 @@ internal sealed class Sender(
     CancellationToken cancellationToken = default)
   {
     ArgumentNullException.ThrowIfNull(command);
-    Type requestType = command.GetType();
-    if (!_commandHandlerWrappers.TryGetValue(requestType, out BaseHandlerWrapper? wrapper))
+    Type commandType = command.GetType();
+    if (!_handlerWrappers.TryGetValue(commandType, out BaseHandlerWrapper? wrapper))
     {
-      throw new InvalidOperationException($"No command handler wrapper found for command of type {requestType}.");
+      throw new InvalidOperationException($"No command handler wrapper found for command of type {commandType}.");
     }
 
     return ((CommandHandlerWrapper<TResponse>)wrapper)
       .HandleAsync(command, _serviceProvider, cancellationToken);
+  }
+
+  public Task<TResponse> SendAsync<TResponse>(
+    IQuery<TResponse> query,
+    CancellationToken cancellationToken = default)
+  {
+    ArgumentNullException.ThrowIfNull(query);
+    Type queryType = query.GetType();
+    if (!_handlerWrappers.TryGetValue(queryType, out BaseHandlerWrapper? wrapper))
+    {
+      throw new InvalidOperationException($"No query handler wrapper found for query of type {queryType}.");
+    }
+
+    return ((QueryHandlerWrapper<TResponse>)wrapper)
+      .HandleAsync(query, _serviceProvider, cancellationToken);
   }
 }
