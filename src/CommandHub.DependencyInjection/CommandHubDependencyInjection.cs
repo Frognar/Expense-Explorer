67fc7a6b24fd30251915a5d2ca082e0940d6c09d
frognar <sebastian.przyszlak@gmail.com>
2024-04-03 08:58:46 +0200

    Fix scope issues

--- src/CommandHub.DependencyInjection/CommandHubDependencyInjection.cs
+++ src/CommandHub.DependencyInjection/CommandHubDependencyInjection.cs
@@ -9,7 +9,13 @@ public static class CommandHubDependencyInjection
   public static void AddCommandHub(this IServiceCollection services, params Assembly[] assemblies)
   {
     services.RegisterHandlers(assemblies);
-    services.AddSingleton<ISender>(sp => CommandRegistry.CreateSender(sp.GetRequiredService, assemblies));
+    services.AddSingleton<ISender>(sp => CommandRegistry.CreateSender(GetRequiredService(sp), assemblies));
+  }
+
+  private static Func<Type, object> GetRequiredService(IServiceProvider sp)
+  {
+    IServiceScope scope = sp.GetRequiredService<IServiceScopeFactory>().CreateScope();
+    return type => scope.ServiceProvider.GetRequiredService(type);
   }
 
   private static void RegisterHandlers(this IServiceCollection services, Assembly[] assemblies)
