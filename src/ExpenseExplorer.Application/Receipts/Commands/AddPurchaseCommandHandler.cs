93b71fbda43e02d31f4d68b09e3b17f05d64e00f
frognar <sebastian.przyszlak@gmail.com>
2024-04-04 21:37:14 +0200

    Refactor AddPurchaseCommandHandler

--- src/ExpenseExplorer.Application/Receipts/Commands/AddPurchaseCommandHandler.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/AddPurchaseCommandHandler.cs
@@ -18,20 +18,43 @@ public class AddPurchaseCommandHandler(IReceiptRepository receiptRepository)
     CancellationToken cancellationToken = default)
   {
     ArgumentNullException.ThrowIfNull(command);
-    Validated<Purchase> validated = PurchaseValidator.Validate(command);
-    Either<Failure, Purchase> purchase = validated.ToEither();
-    Either<Failure, Receipt> receipt = await _receiptRepository.GetAsync(
-      Id.Create(command.ReceiptId),
-      cancellationToken);
+    var eitherFailureOrPurchase = PurchaseValidator.Validate(command).ToEither();
+    var eitherFailureOrReceipt = await AddPurchaseAsync(eitherFailureOrPurchase, command.ReceiptId, cancellationToken);
+    eitherFailureOrReceipt = await SaveAsync(eitherFailureOrReceipt, cancellationToken);
+    return eitherFailureOrReceipt;
+  }
 
-    return await purchase
-      .FlatMapRight(e => receipt.MapRight(r => r.AddPurchase(e)))
-      .FlatMapRightAsync(r => SaveAsync(r, cancellationToken));
+  private async Task<Either<Failure, Receipt>> AddPurchaseAsync(
+    Either<Failure, Purchase> eitherFailureOrPurchase,
+    string receiptId,
+    CancellationToken cancellationToken)
+  {
+    return await eitherFailureOrPurchase.Match(
+      failure => Task.FromResult(Either<Failure, Receipt>.Left(failure)),
+      purchase => AddPurchaseAsync(purchase, Id.Create(receiptId), cancellationToken));
+  }
+
+  private async Task<Either<Failure, Receipt>> AddPurchaseAsync(
+    Purchase purchase,
+    Id receiptId,
+    CancellationToken cancellationToken)
+  {
+    var eitherFailureOrReceipt = await _receiptRepository.GetAsync(receiptId, cancellationToken);
+    return eitherFailureOrReceipt.MapRight(r => r.AddPurchase(purchase));
+  }
+
+  private async Task<Either<Failure, Receipt>> SaveAsync(
+    Either<Failure, Receipt> eitherFailureOrReceipt,
+    CancellationToken cancellationToken)
+  {
+    return await eitherFailureOrReceipt.Match(
+      failure => Task.FromResult(Left.From<Failure, Receipt>(failure)),
+      receipt => SaveAsync(receipt, cancellationToken));
   }
 
   private async Task<Either<Failure, Receipt>> SaveAsync(Receipt receipt, CancellationToken cancellationToken)
   {
-    var result = await _receiptRepository.SaveAsync(receipt, cancellationToken);
-    return result.MapRight(_ => receipt.ClearChanges());
+    var eitherFailureOrVersion = await _receiptRepository.SaveAsync(receipt, cancellationToken);
+    return eitherFailureOrVersion.MapRight(_ => receipt.ClearChanges());
   }
 }
