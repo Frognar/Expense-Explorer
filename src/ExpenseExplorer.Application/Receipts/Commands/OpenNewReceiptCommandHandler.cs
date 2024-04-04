27be0ea89202ce6e5a0d059f6185e8cf9231a70e
frognar <sebastian.przyszlak@gmail.com>
2024-04-04 21:14:20 +0200

    Refactor OpenNewReceiptCommandHandler

--- src/ExpenseExplorer.Application/Receipts/Commands/OpenNewReceiptCommandHandler.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/OpenNewReceiptCommandHandler.cs
@@ -6,6 +6,7 @@ using ExpenseExplorer.Application.Monads;
 using ExpenseExplorer.Application.Receipts.Persistence;
 using ExpenseExplorer.Application.Validations;
 using ExpenseExplorer.Domain.Receipts;
+using ExpenseExplorer.Domain.ValueObjects;
 
 public class OpenNewReceiptCommandHandler(IReceiptRepository receiptRepository)
   : ICommandHandler<OpenNewReceiptCommand, Either<Failure, Receipt>>
@@ -18,13 +19,22 @@ public class OpenNewReceiptCommandHandler(IReceiptRepository receiptRepository)
   {
     ArgumentNullException.ThrowIfNull(command);
     Validated<Receipt> validated = ReceiptValidator.Validate(command);
-    Either<Failure, Receipt> either = validated.ToEither();
-    return await either.FlatMapRightAsync(r => SaveAsync(r, cancellationToken));
+    Either<Failure, Receipt> receipt = await SaveAsync(validated.ToEither(), cancellationToken);
+    return receipt;
+  }
+
+  private async Task<Either<Failure, Receipt>> SaveAsync(
+    Either<Failure, Receipt> either,
+    CancellationToken cancellationToken)
+  {
+    return await either.Match(
+      left => Task.FromResult(Left.From<Failure, Receipt>(left)),
+      right => SaveAsync(right, cancellationToken));
   }
 
   private async Task<Either<Failure, Receipt>> SaveAsync(Receipt receipt, CancellationToken cancellationToken)
   {
-    var result = await _receiptRepository.SaveAsync(receipt, cancellationToken);
+    Either<Failure, Version> result = await _receiptRepository.SaveAsync(receipt, cancellationToken);
     return result.MapRight(_ => receipt.ClearChanges());
   }
 }
