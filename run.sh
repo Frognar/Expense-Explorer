bcbd12f935c3fcc3a6a3e812448a1025f29e9919
frognar <sebastian.przyszlak@gmail.com>
2024-04-06 10:18:40 +0200

    Update run.sh to run postgres db in docker

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker compose -f res/eventstore/docker-compose.yaml up -d
+docker compose -f res/postgresql/docker-compose.yaml up -d
 dotnet src/ExpenseExplorer.API/bin/Release/net8.0/ExpenseExplorer.API.dll
 docker compose -f res/eventstore/docker-compose.yaml stop
+docker compose -f res/postgresql/docker-compose.yaml stop
