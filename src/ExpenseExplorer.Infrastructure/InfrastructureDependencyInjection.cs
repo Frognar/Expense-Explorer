f501c3fe15a964544a266df95b4d48f2f393e289
frognar <sebastian.przyszlak@gmail.com>
2024-04-06 07:52:03 +0200

    Register Postgres in DI

--- src/ExpenseExplorer.Infrastructure/InfrastructureDependencyInjection.cs
+++ src/ExpenseExplorer.Infrastructure/InfrastructureDependencyInjection.cs
@@ -3,6 +3,8 @@ namespace ExpenseExplorer.Infrastructure;
 using ExpenseExplorer.Application;
 using ExpenseExplorer.Application.Receipts.Persistence;
 using ExpenseExplorer.Infrastructure.Receipts.Persistence;
+using ExpenseExplorer.Infrastructure.Receipts.Projections;
+using ExpenseExplorer.Infrastructure.Receipts.Projections.FactHandlers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,11 +12,31 @@ public static class InfrastructureDependencyInjection
 {
   public static void AddInfrastructure(this IServiceCollection serviceCollection, IConfiguration configuration)
   {
-    string? connectionString = configuration.GetConnectionString("EventStore");
-    ArgumentNullException.ThrowIfNull(connectionString);
+    serviceCollection.AddEventStore(configuration);
+    serviceCollection.AddProjectionDb(configuration);
     serviceCollection.AddSingleton<InMemoryMessageQueue>();
     serviceCollection.AddSingleton<IFactBus, FactBus>();
-    serviceCollection.AddScoped<IReceiptRepository>(_ => new EventStoreReceiptRepository(connectionString));
     serviceCollection.AddHostedService<FactProcessorJob>();
+  }
+
+  private static void AddEventStore(this IServiceCollection serviceCollection, IConfiguration configuration)
+  {
+    string? connectionString = configuration.GetConnectionString("EventStore");
+    ArgumentNullException.ThrowIfNull(connectionString);
+    serviceCollection.AddScoped<IReceiptRepository>(_ => new EventStoreReceiptRepository(connectionString));
+  }
+
+  private static void AddProjectionDb(this IServiceCollection serviceCollection, IConfiguration configuration)
+  {
+    string? connectionString = configuration.GetConnectionString("Postgres");
+    ArgumentNullException.ThrowIfNull(connectionString);
+    serviceCollection.AddScoped<ExpenseExplorerContext>(_ => new ExpenseExplorerContext(connectionString));
+
+    serviceCollection.AddProjectionHandlers();
+  }
+
+  private static void AddProjectionHandlers(this IServiceCollection serviceCollection)
+  {
+    serviceCollection.AddTransient<ReceiptCreatedHandler>();
   }
 }
