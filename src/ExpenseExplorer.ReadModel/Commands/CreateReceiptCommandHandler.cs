1dbd39ef4bbf3b1f5eb9eb710ceae7b4c5820178
frognar <sebastian.przyszlak@gmail.com>
2024-04-07 10:49:43 +0200

    Add CreateReceipt command handler for read model

---
+++ src/ExpenseExplorer.ReadModel/Commands/CreateReceiptCommandHandler.cs
@@ -0,0 +1,24 @@
+namespace ExpenseExplorer.ReadModel.Commands;
+
+using System.Diagnostics.CodeAnalysis;
+using CommandHub.Commands;
+using ExpenseExplorer.ReadModel.Models;
+
+[SuppressMessage(
+  "Performance",
+  "CA1812:Avoid uninstantiated internal classes",
+  Justification = "Instantiated by DI container")]
+public sealed class CreateReceiptCommandHandler(ExpenseExplorerContext context)
+  : ICommandHandler<CreateReceiptCommand, Unit>
+{
+  private readonly ExpenseExplorerContext _context = context;
+
+  public async Task<Unit> HandleAsync(CreateReceiptCommand command, CancellationToken cancellationToken = default)
+  {
+    ArgumentNullException.ThrowIfNull(command);
+    _context.ReceiptHeaders.Add(new DbReceiptHeader(command.Id, command.Store, command.PurchaseDate, 0));
+    await _context.SaveChangesAsync(cancellationToken);
+    Console.WriteLine(command);
+    return Unit.Instance;
+  }
+}
