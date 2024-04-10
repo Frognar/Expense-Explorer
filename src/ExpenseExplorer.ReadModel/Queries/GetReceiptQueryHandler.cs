b8a53cd1c30fa686f1a3a5b8e96ed728ab0e3dce
frognar <sebastian.przyszlak@gmail.com>
2024-04-10 06:22:01 +0200

    Read receipts from db in GetReceiptQueryHandler

--- src/ExpenseExplorer.ReadModel/Queries/GetReceiptQueryHandler.cs
+++ src/ExpenseExplorer.ReadModel/Queries/GetReceiptQueryHandler.cs
@@ -3,13 +3,26 @@ namespace ExpenseExplorer.ReadModel.Queries;
 using CommandHub.Queries;
 using ExpenseExplorer.ReadModel.Extensions;
 using ExpenseExplorer.ReadModel.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class GetReceiptQueryHandler : IQueryHandler<GetReceiptQuery, PageOf<ReceiptHeaders>>
+public class GetReceiptQueryHandler(ExpenseExplorerContext context)
+  : IQueryHandler<GetReceiptQuery, PageOf<ReceiptHeaders>>
 {
-  public Task<PageOf<ReceiptHeaders>> HandleAsync(GetReceiptQuery query, CancellationToken cancellationToken = default)
+  private readonly ExpenseExplorerContext _context = context;
+
+  public async Task<PageOf<ReceiptHeaders>> HandleAsync(
+    GetReceiptQuery query,
+    CancellationToken cancellationToken = default)
   {
-    var receipts = Enumerable.Range(0, 10).Select(_ => (ReceiptHeaders)null!);
-    var response = Page.Of(receipts, 15);
-    return Task.FromResult(response);
+    var receipts = await _context.ReceiptHeaders.AsNoTracking()
+      .OrderBy(r => r.PurchaseDate)
+      .Skip(0)
+      .Take(10)
+      .Select(r => new ReceiptHeaders(r.Id, r.Store, r.PurchaseDate, r.Total))
+      .ToListAsync(cancellationToken);
+
+    int totalCount = await _context.ReceiptHeaders.CountAsync(cancellationToken);
+    var response = Page.Of(receipts, totalCount);
+    return response;
   }
 }
