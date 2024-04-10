db7340bcb6dad11c60e297edeb28e484a7032abb
frognar <sebastian.przyszlak@gmail.com>
2024-04-10 06:19:50 +0200

    Add ServiceScopeFactory in BaseIntegrationTest

--- tests/ExpenseExplorer.API.Tests.Integration/BaseIntegrationTest.cs
+++ tests/ExpenseExplorer.API.Tests.Integration/BaseIntegrationTest.cs
@@ -1,6 +1,11 @@
 namespace ExpenseExplorer.API.Tests.Integration;
 
+using Microsoft.Extensions.DependencyInjection;
+
 public abstract class BaseIntegrationTest(ReceiptApiFactory factory) : IClassFixture<ReceiptApiFactory>
 {
   protected HttpClient Client { get; } = factory.CreateClient();
+
+  protected IServiceScopeFactory ServiceScopeFactory { get; }
+    = factory.Services.GetRequiredService<IServiceScopeFactory>();
 }
