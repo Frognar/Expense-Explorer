6981051796ccb6eef7210e4b29b75de96387871e
frognar <sebastian.przyszlak@gmail.com>
2024-04-11 07:03:08 +0200

    Return Failure on DbException while processing GetReceiptQuery

--- src/ExpenseExplorer.ReadModel/Queries/GetReceiptQueryHandler.cs
+++ src/ExpenseExplorer.ReadModel/Queries/GetReceiptQueryHandler.cs
@@ -1,5 +1,6 @@
 namespace ExpenseExplorer.ReadModel.Queries;
 
+using System.Data.Common;
 using CommandHub.Queries;
 using ExpenseExplorer.ReadModel.Extensions;
 using ExpenseExplorer.ReadModel.Models;
@@ -16,15 +17,22 @@ public class GetReceiptQueryHandler(ExpenseExplorerContext context)
     GetReceiptQuery query,
     CancellationToken cancellationToken = default)
   {
-    var receipts = await _context.ReceiptHeaders.AsNoTracking()
-      .OrderBy(r => r.PurchaseDate)
-      .Skip(0)
-      .Take(10)
-      .Select(r => new ReceiptHeaders(r.Id, r.Store, r.PurchaseDate, r.Total))
-      .ToListAsync(cancellationToken);
+    try
+    {
+      var receipts = await _context.ReceiptHeaders.AsNoTracking()
+        .OrderBy(r => r.PurchaseDate)
+        .Skip(0)
+        .Take(10)
+        .Select(r => new ReceiptHeaders(r.Id, r.Store, r.PurchaseDate, r.Total))
+        .ToListAsync(cancellationToken);
 
-    int totalCount = await _context.ReceiptHeaders.CountAsync(cancellationToken);
-    var response = Page.Of(receipts, totalCount);
-    return Right.From<Failure, PageOf<ReceiptHeaders>>(response);
+      int totalCount = await _context.ReceiptHeaders.CountAsync(cancellationToken);
+      var response = Page.Of(receipts, totalCount);
+      return Right.From<Failure, PageOf<ReceiptHeaders>>(response);
+    }
+    catch (DbException ex)
+    {
+      return Left.From<Failure, PageOf<ReceiptHeaders>>(new FatalFailure(ex));
+    }
   }
 }
