118ed5dbae3ca6354e940a67905c12526a625925
frognar <sebastian.przyszlak@gmail.com>
2024-04-14 09:19:55 +0200

    Add OrderingDescriptor struct

---
+++ src/ExpenseExplorer.ReadModel/OrderingDescriptor.cs
@@ -0,0 +1,22 @@
+namespace ExpenseExplorer.ReadModel;
+
+using System.Linq.Expressions;
+
+public readonly record struct OrderingDescriptor<T>(Expression<Func<T, object>> Selector, bool IsDescending = false)
+{
+  internal static OrderingDescriptor<T> AscendingBy(Expression<Func<T, object>> selector) => new(selector);
+
+  internal static OrderingDescriptor<T> DescendingBy(Expression<Func<T, object>> selector) => new(selector, true);
+
+  internal IOrderedQueryable<T> Apply(IQueryable<T> source)
+  {
+    ArgumentNullException.ThrowIfNull(source);
+    return IsDescending ? source.OrderByDescending(Selector) : source.OrderBy(Selector);
+  }
+
+  internal IOrderedQueryable<T> Apply(IOrderedQueryable<T> source)
+  {
+    ArgumentNullException.ThrowIfNull(source);
+    return IsDescending ? source.ThenByDescending(Selector) : source.ThenBy(Selector);
+  }
+}
