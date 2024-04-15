c1aa51197782971739ac2c904bdff52d0dbcb372
frognar <sebastian.przyszlak@gmail.com>
2024-04-15 08:05:42 +0200

    Add extension method for filtering IQueryable<T>

--- src/ExpenseExplorer.ReadModel/QueryableExtensions.cs
+++ src/ExpenseExplorer.ReadModel/QueryableExtensions.cs
@@ -1,5 +1,7 @@
 namespace ExpenseExplorer.ReadModel;
 
+using System.Linq.Expressions;
+
 public static class QueryableExtensions
 {
   public static IQueryable<T> GetPage<T>(this IQueryable<T> source, int pageNumber, int pageSize)
@@ -16,5 +18,15 @@ public static class QueryableExtensions
     ArgumentNullException.ThrowIfNull(source);
     IOrderedQueryable<T> ordered = selector.Apply(source);
     return selectors.Aggregate(ordered, (current, s) => s.Apply(current));
+  }
+
+  public static IQueryable<T> Filter<T>(
+    this IQueryable<T> source,
+    Expression<Func<T, bool>> predicate,
+    params Expression<Func<T, bool>>[] predicates)
+  {
+    ArgumentNullException.ThrowIfNull(source);
+    IQueryable<T> filtered = source.Where(predicate);
+    return predicates.Aggregate(filtered, (current, p) => current.Where(p));
   }
 }
