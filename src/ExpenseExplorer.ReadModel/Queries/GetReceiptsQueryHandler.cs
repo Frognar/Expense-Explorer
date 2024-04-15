3c2e034eabaecf52ec0074ae3e4af7e8be0858f1
frognar <sebastian.przyszlak@gmail.com>
2024-04-15 08:06:09 +0200

    Refactor filtering receipt headers

--- src/ExpenseExplorer.ReadModel/Queries/GetReceiptsQueryHandler.cs
+++ src/ExpenseExplorer.ReadModel/Queries/GetReceiptsQueryHandler.cs
@@ -1,6 +1,7 @@
 namespace ExpenseExplorer.ReadModel.Queries;
 
 using System.Data.Common;
+using System.Linq.Expressions;
 using CommandHub.Queries;
 using ExpenseExplorer.ReadModel.Extensions;
 using ExpenseExplorer.ReadModel.Models;
@@ -21,19 +22,11 @@ public class GetReceiptsQueryHandler(ExpenseExplorerContext context)
     try
     {
       ArgumentNullException.ThrowIfNull(query);
-      string search = query.Search.ToUpperInvariant();
       List<ReceiptHeaders> receipts = await _context.ReceiptHeaders.AsNoTracking()
-        .Where(r => r.PurchaseDate >= query.After && r.PurchaseDate <= query.Before)
-        .Where(r => r.Total >= query.MinTotal && r.Total <= query.MaxTotal)
-
-        // EFCore does not support StringComparison and CultureInfo enums in LINQ queries
-#pragma warning disable CA1304
-#pragma warning disable CA1311
-#pragma warning disable CA1862
-        .Where(r => r.Store.ToUpper().Contains(search))
-#pragma warning restore CA1862
-#pragma warning restore CA1311
-#pragma warning restore CA1304
+        .Filter(
+          PurchaseDateBetween(query.After, query.Before),
+          TotalBetween(query.MinTotal, query.MaxTotal),
+          StoreContains(query.Search))
         .OrderByMany(
           Order.AscendingBy<DbReceiptHeader>(r => r.PurchaseDate),
           Order.DescendingBy<DbReceiptHeader>(r => r.Id))
@@ -49,5 +42,34 @@ public class GetReceiptsQueryHandler(ExpenseExplorerContext context)
     {
       return Left.From<Failure, PageOf<ReceiptHeaders>>(new FatalFailure(ex));
     }
+  }
+
+  private static Expression<Func<DbReceiptHeader, bool>> PurchaseDateBetween(DateOnly min, DateOnly max)
+  {
+    return r => r.PurchaseDate >= min && r.PurchaseDate <= max;
+  }
+
+  private static Expression<Func<DbReceiptHeader, bool>> TotalBetween(decimal min, decimal max)
+  {
+    return r => r.Total >= min && r.Total <= max;
+  }
+
+  private static Expression<Func<DbReceiptHeader, bool>> StoreContains(string search)
+  {
+    if (string.IsNullOrWhiteSpace(search))
+    {
+      return r => true;
+    }
+
+    search = search.ToUpperInvariant();
+
+    // EFCore does not support StringComparison and CultureInfo enums in LINQ queries
+#pragma warning disable CA1304
+#pragma warning disable CA1311
+#pragma warning disable CA1862
+    return r => r.Store.ToUpper().Contains(search);
+#pragma warning restore CA1862
+#pragma warning restore CA1311
+#pragma warning restore CA1304
   }
 }
