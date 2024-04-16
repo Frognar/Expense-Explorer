e23d66304a261a28ead13e1b03d0edf530765895
frognar <sebastian.przyszlak@gmail.com>
2024-04-16 06:01:44 +0200

    Add TryCreate methods to Store and PurchaseDate models

--- src/ExpenseExplorer.Domain/ValueObjects/PurchaseDate.cs
+++ src/ExpenseExplorer.Domain/ValueObjects/PurchaseDate.cs
@@ -2,6 +2,7 @@ namespace ExpenseExplorer.Domain.ValueObjects;
 
 using System.Text.Json.Serialization;
 using ExpenseExplorer.Domain.Exceptions;
+using FunctionalCore.Monads;
 
 public record PurchaseDate
 {
@@ -24,5 +25,12 @@ public record PurchaseDate
   public static PurchaseDate Create(DateOnly date, DateOnly today)
   {
     return new PurchaseDate(date, today);
+  }
+
+  public static Maybe<PurchaseDate> TryCreate(DateOnly date, DateOnly today)
+  {
+    return date > today
+      ? None.OfType<PurchaseDate>()
+      : Some.From(new PurchaseDate(date, today));
   }
 }
--- src/ExpenseExplorer.Domain/ValueObjects/Store.cs
+++ src/ExpenseExplorer.Domain/ValueObjects/Store.cs
@@ -2,6 +2,7 @@ namespace ExpenseExplorer.Domain.ValueObjects;
 
 using System.Text.Json.Serialization;
 using ExpenseExplorer.Domain.Exceptions;
+using FunctionalCore.Monads;
 
 public record Store
 {
@@ -17,5 +18,12 @@ public record Store
   public static Store Create(string name)
   {
     return new Store(name);
+  }
+
+  public static Maybe<Store> TryCreate(string name)
+  {
+    return string.IsNullOrWhiteSpace(name)
+      ? None.OfType<Store>()
+      : Some.From(new Store(name));
   }
 }
