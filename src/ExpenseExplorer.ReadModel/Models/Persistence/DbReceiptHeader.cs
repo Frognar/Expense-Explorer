7e16ab0cade5cb89b2ace850710061a20caeaec0
frognar <sebastian.przyszlak@gmail.com>
2024-04-17 06:55:56 +0200

    Add string length limitations in DbReceiptHeader

--- src/ExpenseExplorer.ReadModel/Models/Persistence/DbReceiptHeader.cs
+++ src/ExpenseExplorer.ReadModel/Models/Persistence/DbReceiptHeader.cs
@@ -1,5 +1,6 @@
 namespace ExpenseExplorer.ReadModel.Models.Persistence;
 
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 
 [SuppressMessage(
@@ -8,8 +9,10 @@ using System.Diagnostics.CodeAnalysis;
   Justification = "Instantiated by Entity Framework")]
 public sealed class DbReceiptHeader(string id, string store, DateOnly purchaseDate, decimal total)
 {
+  [MaxLength(64)]
   public string Id { get; init; } = id;
 
+  [MaxLength(128)]
   public string Store { get; init; } = store;
 
   public DateOnly PurchaseDate { get; init; } = purchaseDate;
