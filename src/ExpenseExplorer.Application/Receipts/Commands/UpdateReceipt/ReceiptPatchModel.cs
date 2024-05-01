d1975c45e720e1f6c62efd5c95ec13d3ff978171
frognar <sebastian.przyszlak@gmail.com>
2024-05-01 16:21:44 +0200

    Change ReceiptPatchModel to readonly record struct

--- src/ExpenseExplorer.Application/Receipts/Commands/UpdateReceipt/ReceiptPatchModel.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/UpdateReceipt/ReceiptPatchModel.cs
@@ -3,7 +3,7 @@ namespace ExpenseExplorer.Application.Receipts.Commands;
 using ExpenseExplorer.Domain.ValueObjects;
 using FunctionalCore.Monads;
 
-internal sealed record ReceiptPatchModel(Maybe<Store> Store, Maybe<PurchaseDate> PurchaseDate, DateOnly Today)
+internal readonly record struct ReceiptPatchModel(Maybe<Store> Store, Maybe<PurchaseDate> PurchaseDate, DateOnly Today)
 {
   public static ReceiptPatchModel Create(Maybe<Store> store, Maybe<PurchaseDate> purchaseDate, DateOnly today)
     => new(store, purchaseDate, today);
