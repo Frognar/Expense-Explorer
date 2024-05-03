a558cb769e2298266e02c349797a5327b2331822
frognar <sebastian.przyszlak@gmail.com>
2024-05-03 09:55:34 +0200

    Remove Item.Create method and delete EmptyItemNameException

--- src/ExpenseExplorer.Domain/ValueObjects/Item.cs
+++ src/ExpenseExplorer.Domain/ValueObjects/Item.cs
@@ -1,38 +1,20 @@
 namespace ExpenseExplorer.Domain.ValueObjects;
 
-using ExpenseExplorer.Domain.Exceptions;
 using FunctionalCore.Monads;
 
-public readonly record struct Item(string Name)
+public readonly record struct Item
 {
-  private readonly string _name = TrimOrThrow(Name);
-
-  public string Name
+  private Item(string name)
   {
-    get => _name;
-    init => _name = TrimOrThrow(value);
+    Name = name.Trim();
   }
 
-  public static Item Create(string name)
-  {
-    return new Item(name);
-  }
+  public string Name { get; }
 
   public static Maybe<Item> TryCreate(string name)
   {
     return string.IsNullOrWhiteSpace(name)
       ? None.OfType<Item>()
       : Some.From(new Item(name));
-  }
-
-  private static string TrimOrThrow(string name)
-  {
-    ArgumentNullException.ThrowIfNull(name);
-    return IsValid(name) ? name.Trim() : throw new EmptyItemNameException();
-  }
-
-  private static bool IsValid(string name)
-  {
-    return !string.IsNullOrWhiteSpace(name);
   }
 }
--- tests/ExpenseExplorer.Domain.Tests/ItemTests.cs
+++ tests/ExpenseExplorer.Domain.Tests/ItemTests.cs
@@ -2,30 +2,21 @@ namespace ExpenseExplorer.Domain.Tests;
 
 public class ItemTests
 {
-  [Property(Arbitrary = [typeof(EmptyOrWhiteSpaceStringGenerator)])]
-  public void ThrowsExceptionWhenNameIsEmpty(string name)
-  {
-    Action act = () => _ = Item.Create(name);
-    act.Should().Throw<EmptyItemNameException>();
-  }
-
   [Property(Arbitrary = [typeof(NonEmptyStringGenerator)])]
   public void NameIsTrimmed(string name)
   {
-    Item.Create(name).Name.Should().Be(name.Trim());
-  }
-
-  [Property(Arbitrary = [typeof(NonEmptyStringGenerator)])]
-  public void NameIsTrimmedWithRecordSyntax(string name)
-  {
-    Item item = Item.Create("123") with { Name = name };
-    item.Name.Should().Be(name.Trim());
+    Item.TryCreate(name)
+      .Match(() => " [EMPTY] ", i => i.Name)
+      .Should()
+      .Be(name.Trim());
   }
 
   [Property(Arbitrary = [typeof(EmptyOrWhiteSpaceStringGenerator)])]
-  public void ThrowsExceptionWhenNameIsEmptyWithRecordSyntax(string name)
+  public void ReturnsNoneWhenNameIsEmpty(string name)
   {
-    Action act = () => _ = Item.Create("123") with { Name = name };
-    act.Should().Throw<EmptyItemNameException>();
+    Item.TryCreate(name)
+      .Match(() => " [EMPTY] ", i => i.Name)
+      .Should()
+      .Be(" [EMPTY] ");
   }
 }
