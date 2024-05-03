0e5a53311d8151783bdf2d8b587629cae58c8a51
frognar <sebastian.przyszlak@gmail.com>
2024-05-03 10:21:08 +0200

    Update patch models to have get-only properties

--- src/ExpenseExplorer.Application/Receipts/Commands/UpdatePurchaseDetails/PurchasePatchModel.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/UpdatePurchaseDetails/PurchasePatchModel.cs
@@ -3,14 +3,36 @@ namespace ExpenseExplorer.Application.Receipts.Commands;
 using ExpenseExplorer.Domain.ValueObjects;
 using FunctionalCore.Monads;
 
-internal readonly record struct PurchasePatchModel(
-  Maybe<Item> Item,
-  Maybe<Category> Category,
-  Maybe<Quantity> Quantity,
-  Maybe<Money> UnitPrice,
-  Maybe<Money> TotalDiscount,
-  Maybe<Description> Description)
+internal readonly record struct PurchasePatchModel
 {
+  private PurchasePatchModel(
+    Maybe<Item> item,
+    Maybe<Category> category,
+    Maybe<Quantity> quantity,
+    Maybe<Money> unitPrice,
+    Maybe<Money> totalDiscount,
+    Maybe<Description> description)
+  {
+    Item = item;
+    Category = category;
+    Quantity = quantity;
+    UnitPrice = unitPrice;
+    TotalDiscount = totalDiscount;
+    Description = description;
+  }
+
+  public Maybe<Item> Item { get; }
+
+  public Maybe<Category> Category { get; }
+
+  public Maybe<Quantity> Quantity { get; }
+
+  public Maybe<Money> UnitPrice { get; }
+
+  public Maybe<Money> TotalDiscount { get; }
+
+  public Maybe<Description> Description { get; }
+
   public static PurchasePatchModel Create(
     Maybe<Item> item,
     Maybe<Category> category,
--- src/ExpenseExplorer.Application/Receipts/Commands/UpdateReceipt/ReceiptPatchModel.cs
+++ src/ExpenseExplorer.Application/Receipts/Commands/UpdateReceipt/ReceiptPatchModel.cs
@@ -3,8 +3,21 @@ namespace ExpenseExplorer.Application.Receipts.Commands;
 using ExpenseExplorer.Domain.ValueObjects;
 using FunctionalCore.Monads;
 
-internal readonly record struct ReceiptPatchModel(Maybe<Store> Store, Maybe<PurchaseDate> PurchaseDate, DateOnly Today)
+internal readonly record struct ReceiptPatchModel
 {
+  private ReceiptPatchModel(Maybe<Store> store, Maybe<PurchaseDate> purchaseDate, DateOnly today)
+  {
+    Store = store;
+    PurchaseDate = purchaseDate;
+    Today = today;
+  }
+
+  public Maybe<Store> Store { get; }
+
+  public Maybe<PurchaseDate> PurchaseDate { get; }
+
+  public DateOnly Today { get; }
+
   public static ReceiptPatchModel Create(Maybe<Store> store, Maybe<PurchaseDate> purchaseDate, DateOnly today)
     => new(store, purchaseDate, today);
 }
