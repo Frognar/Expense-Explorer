bb620b08b82b945a25ecc52838b2c9c4f8c348c4
frognar <sebastian.przyszlak@gmail.com>
2024-05-04 07:30:15 +0200

    Remove Concat from ValidationFailure

--- src/FunctionalCore/Failures/ValidationFailure.cs
+++ src/FunctionalCore/Failures/ValidationFailure.cs
@@ -1,11 +1,4 @@
 namespace FunctionalCore.Failures;
 
 public record ValidationFailure(IEnumerable<ValidationError> Errors)
-  : Failure("One or more validation errors occurred.")
-{
-  public ValidationFailure Concat(ValidationFailure other)
-  {
-    ArgumentNullException.ThrowIfNull(other);
-    return Validation(Errors.Concat(other.Errors));
-  }
-}
+  : Failure("One or more validation errors occurred.");
