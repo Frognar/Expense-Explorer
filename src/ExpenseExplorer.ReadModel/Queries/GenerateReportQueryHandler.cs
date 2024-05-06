3b46167b7a44e5cd3a091b3e031fae939c719279
frognar <sebastian.przyszlak@gmail.com>
2024-05-06 06:10:24 +0200

    Implement handler for generating reports

---
+++ src/ExpenseExplorer.ReadModel/Queries/GenerateReportQueryHandler.cs
@@ -0,0 +1,36 @@
+namespace ExpenseExplorer.ReadModel.Queries;
+
+using System.Data.Common;
+using CommandHub.Queries;
+using ExpenseExplorer.ReadModel.Models;
+using FunctionalCore.Failures;
+using FunctionalCore.Monads;
+using Microsoft.EntityFrameworkCore;
+
+public sealed class GenerateReportQueryHandler(ExpenseExplorerContext context)
+  : IQueryHandler<GenerateReportQuery, Result<Report>>
+{
+  private readonly ExpenseExplorerContext _context = context;
+
+  public async Task<Result<Report>> HandleAsync(
+    GenerateReportQuery query,
+    CancellationToken cancellationToken = default)
+  {
+    try
+    {
+      Dictionary<string, decimal> data = await _context.Receipts.AsNoTracking()
+        .Where(r => r.PurchaseDate >= query.From && r.PurchaseDate <= query.To)
+        .SelectMany(r => r.Purchases)
+        .Select(p => new { p.Category, TotalPrice = (p.Quantity * p.UnitPrice) - p.TotalDiscount })
+        .GroupBy(d => d.Category)
+        .ToDictionaryAsync(d => d.Key, d => d.Sum(p => p.TotalPrice), cancellationToken);
+
+      Report report = new(data);
+      return Success.From(report);
+    }
+    catch (DbException ex)
+    {
+      return Fail.OfType<Report>(Failure.Fatal(ex));
+    }
+  }
+}
