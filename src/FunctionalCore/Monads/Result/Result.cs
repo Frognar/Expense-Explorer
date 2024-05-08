6c68cb008f802c2575963211ffd677d274b2143d
frognar <sebastian.przyszlak@gmail.com>
2024-05-08 06:10:01 +0200

    Add MatchAsync to Result and move MapAsync and FlatMapAsync from extensions to Result

--- src/FunctionalCore/Monads/Result/Result.cs
+++ src/FunctionalCore/Monads/Result/Result.cs
@@ -22,16 +22,44 @@ public sealed class Result<T>
     };
   }
 
+  public async Task<TResult> MatchAsync<TResult>(Func<Failure, Task<TResult>> failure, Func<T, Task<TResult>> success)
+  {
+    ArgumentNullException.ThrowIfNull(failure);
+    ArgumentNullException.ThrowIfNull(success);
+    return _resultType switch
+    {
+      FailureType failureType => await failure(failureType.Failure).ConfigureAwait(false),
+      SuccessType successType => await success(successType.Value).ConfigureAwait(false),
+      _ => throw new InvalidOperationException("Unknown result type."),
+    };
+  }
+
   public Result<TResult> Map<TResult>(Func<T, TResult> map)
   {
     ArgumentNullException.ThrowIfNull(map);
     return Match(Result<TResult>.Fail, value => Result<TResult>.Success(map(value)));
   }
 
+  public async Task<Result<TResult>> MapAsync<TResult>(Func<T, Task<TResult>> map)
+  {
+    ArgumentNullException.ThrowIfNull(map);
+    return await MatchAsync(
+      failure => Task.FromResult(Result<TResult>.Fail(failure)),
+      async value => Result<TResult>.Success(await map(value)));
+  }
+
   public Result<TResult> FlatMap<TResult>(Func<T, Result<TResult>> map)
   {
     ArgumentNullException.ThrowIfNull(map);
     return Match(Result<TResult>.Fail, map);
+  }
+
+  public async Task<Result<TResult>> FlatMapAsync<TResult>(Func<T, Task<Result<TResult>>> map)
+  {
+    ArgumentNullException.ThrowIfNull(map);
+    return await MatchAsync(
+      failure => Task.FromResult(Result<TResult>.Fail(failure)),
+      map);
   }
 
   internal static Result<T> Success(T value) => new(new SuccessType(value));
--- src/FunctionalCore/Monads/Result/ResultExtensions.cs
+++ src/FunctionalCore/Monads/Result/ResultExtensions.cs
@@ -3,15 +3,6 @@ namespace FunctionalCore.Monads;
 #pragma warning disable VSTHRD003 // Avoid awaiting foreign Tasks - Required to enable seamless LINQ integration with asynchronous operations
 public static class ResultExtensions
 {
-  public static async Task<Result<TResult>> MapAsync<T, TResult>(this Result<T> source, Func<T, Task<TResult>> map)
-  {
-    ArgumentNullException.ThrowIfNull(source);
-    ArgumentNullException.ThrowIfNull(map);
-    return await source.Match(
-      failure => Task.FromResult(Fail.OfType<TResult>(failure)),
-      async value => Success.From(await map(value)));
-  }
-
   public static async Task<Result<TResult>> MapAsync<T, TResult>(
     this Task<Result<T>> source,
     Func<T, Task<TResult>> map)
@@ -28,17 +19,6 @@ public static class ResultExtensions
     ArgumentNullException.ThrowIfNull(source);
     ArgumentNullException.ThrowIfNull(map);
     return (await source).Map(map);
-  }
-
-  public static async Task<Result<TResult>> FlatMapAsync<T, TResult>(
-    this Result<T> source,
-    Func<T, Task<Result<TResult>>> map)
-  {
-    ArgumentNullException.ThrowIfNull(source);
-    ArgumentNullException.ThrowIfNull(map);
-    return await source.Match(
-      failure => Task.FromResult(Fail.OfType<TResult>(failure)),
-      async value => await map(value));
   }
 
   public static async Task<Result<TResult>> FlatMapAsync<T, TResult>(
