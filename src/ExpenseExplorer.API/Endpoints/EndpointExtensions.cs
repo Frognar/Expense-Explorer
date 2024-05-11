7c0fc1d7badd4f3de9eca81dbd8dc0568203b1d7
frognar <sebastian.przyszlak@gmail.com>
2024-05-11 13:08:00 +0200

    Register item endpoints in API

--- src/ExpenseExplorer.API/Endpoints/EndpointExtensions.cs
+++ src/ExpenseExplorer.API/Endpoints/EndpointExtensions.cs
@@ -7,6 +7,7 @@ public static class EndpointExtensions
     return endpointRouteBuilder
       .MapReceiptEndpoints()
       .MapStoreEndpoints()
+      .MapItemEndpoints()
       .MapReportEndpoints();
   }
 }
