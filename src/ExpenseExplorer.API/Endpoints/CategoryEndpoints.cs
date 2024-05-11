8d984dcc24f06a7179d519b3cf17f8b5ada35c8f
frognar <sebastian.przyszlak@gmail.com>
2024-05-11 13:09:41 +0200

    Create category endpoints

---
+++ src/ExpenseExplorer.API/Endpoints/CategoryEndpoints.cs
@@ -0,0 +1,29 @@
+namespace ExpenseExplorer.API.Endpoints;
+
+using CommandHub;
+using ExpenseExplorer.API.Mappers;
+using ExpenseExplorer.ReadModel.Queries;
+using static ExpenseExplorer.API.Endpoints.FailureHandler;
+
+public static class CategoryEndpoints
+{
+  public static IEndpointRouteBuilder MapCategoryEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+  {
+    RouteGroupBuilder group = endpointRouteBuilder.MapGroup("/api/categories");
+    group.MapGet("/", GetCategoriesAsync);
+    return endpointRouteBuilder;
+  }
+
+  private static async Task<IResult> GetCategoriesAsync(
+    int? pageSize,
+    int? pageNumber,
+    string? search,
+    ISender sender,
+    CancellationToken cancellationToken = default)
+  {
+    GetCategoriesQuery query = new(pageSize, pageNumber, search);
+    return (await sender.SendAsync(query, cancellationToken))
+      .Map(page => page.MapToResponse())
+      .Match(Handle, Results.Ok);
+  }
+}
