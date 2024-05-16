b1b12600a92b32ea345a1ba634ff7f6ebfed626d
frognar <sebastian.przyszlak@gmail.com>
2024-05-16 07:32:32 +0200

    Implement income repository

--- src/ExpenseExplorer.Infrastructure/Incomes/Persistence/EventStoreIncomeRepository.cs
+++ src/ExpenseExplorer.Infrastructure/Incomes/Persistence/EventStoreIncomeRepository.cs
@@ -1,8 +1,10 @@
 namespace ExpenseExplorer.Infrastructure.Incomes.Persistence;
 
 using ExpenseExplorer.Application.Incomes.Persistence;
+using ExpenseExplorer.Domain.Facts;
 using ExpenseExplorer.Domain.Incomes;
 using ExpenseExplorer.Domain.ValueObjects;
+using ExpenseExplorer.Infrastructure.Exceptions;
 using FunctionalCore.Failures;
 using FunctionalCore.Monads;
 
@@ -15,13 +17,37 @@ public sealed class EventStoreIncomeRepository(string connectionString) : IIncom
     _eventStore.Dispose();
   }
 
-  public Task<Result<Version>> SaveAsync(Income income, CancellationToken cancellationToken)
+  public async Task<Result<Version>> SaveAsync(Income income, CancellationToken cancellationToken)
   {
-    return Task.FromResult(Fail.OfType<Version>(Failure.Fatal(new NotImplementedException())));
+    try
+    {
+      ArgumentNullException.ThrowIfNull(income);
+      Version version = await _eventStore.SaveEventsAsync(
+        income.Id,
+        income.Version,
+        income.UnsavedChanges,
+        cancellationToken);
+
+      return Success.From(version);
+    }
+    catch (FactSaveException ex)
+    {
+      return Fail.OfType<Version>(Failure.Fatal(ex));
+    }
   }
 
-  public Task<Result<Income>> GetAsync(Id id, CancellationToken cancellationToken)
+  public async Task<Result<Income>> GetAsync(Id id, CancellationToken cancellationToken)
   {
-    return Task.FromResult(Fail.OfType<Income>(Failure.Fatal(new NotImplementedException())));
+    try
+    {
+      (List<Fact> facts, Version version) = await _eventStore.GetEventsAsync(id, cancellationToken);
+      return facts.Count == 0
+        ? Fail.OfType<Income>(Failure.NotFound("Income not found", id.Value))
+        : Income.Recreate(facts, version);
+    }
+    catch (FactSaveException ex)
+    {
+      return Fail.OfType<Income>(Failure.Fatal(ex));
+    }
   }
 }
