4899148cf15c477db370779f4d78c1cb975a856f
frognar <sebastian.przyszlak@gmail.com>
2024-05-20 07:30:23 +0200

    Migration Income

--- src/ExpenseExplorer.ReadModel/Migrations/20240520052919_Income.Designer.cs
+++ src/ExpenseExplorer.ReadModel/Migrations/20240520052919_Income.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ExpenseExplorer.ReadModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ExpenseExplorer.ReadModel.Migrations
 {
     [DbContext(typeof(ExpenseExplorerContext))]
-    partial class ExpenseExplorerContextModelSnapshot : ModelSnapshot
+    [Migration("20240520052919_Income")]
+    partial class Income
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
