fd97f694a5362a60449a18f6c24e3a91f8b90351
frognar <sebastian.przyszlak@gmail.com>
2024-05-21 06:41:48 +0200

    Convert DbIncome from record to class

--- src/ExpenseExplorer.ReadModel/Models/Persistence/DbIncome.cs
+++ src/ExpenseExplorer.ReadModel/Models/Persistence/DbIncome.cs
@@ -1,9 +1,22 @@
 namespace ExpenseExplorer.ReadModel.Models.Persistence;
 
-public sealed record DbIncome(
-  string Id,
-  string Source,
-  decimal Amount,
-  string Category,
-  DateOnly ReceivedDate,
-  string Description);
+public sealed class DbIncome(
+  string id,
+  string source,
+  decimal amount,
+  string category,
+  DateOnly receivedDate,
+  string description)
+{
+  public string Id { get; init; } = id;
+
+  public string Source { get; set; } = source;
+
+  public decimal Amount { get; set; } = amount;
+
+  public string Category { get; set; } = category;
+
+  public DateOnly ReceivedDate { get; set; } = receivedDate;
+
+  public string Description { get; set; } = description;
+}
