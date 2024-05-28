ef700d4e741af9abe0a77ac049f7ac7403cde410
frognar <sebastian.przyszlak@gmail.com>
2024-05-28 21:30:58 +0200

    Add HttpClient to GUI project DI container

--- src/ExpenseExplorer.GUI/Program.cs
+++ src/ExpenseExplorer.GUI/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
   .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
