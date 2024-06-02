ce769863d53f6cd91deb0100084a72dba927a43a
frognar <sebastian.przyszlak@gmail.com>
2024-06-02 10:05:17 +0200

    Add map methods from ReceiptModel to OpenNewReceiptRequest and UpdateReceiptRequest

--- src/ExpenseExplorer.GUI/Helpers/ReceiptMapper.cs
+++ src/ExpenseExplorer.GUI/Helpers/ReceiptMapper.cs
@@ -1,5 +1,6 @@
 namespace ExpenseExplorer.GUI.Helpers;
 
+using ExpenseExplorer.API.Contract;
 using ExpenseExplorer.API.Contract.ReadModel;
 using ExpenseExplorer.GUI.Data;
 
@@ -12,5 +13,17 @@ public static class ReceiptMapper
     {
       Id = receipt.Id, Store = receipt.Store, PurchaseDate = receipt.PurchaseDate, Total = receipt.Total,
     };
+  }
+
+  public static OpenNewReceiptRequest ToAddRequest(this ReceiptModel receipt)
+  {
+    ArgumentNullException.ThrowIfNull(receipt);
+    return new OpenNewReceiptRequest(receipt.Store, receipt.PurchaseDate);
+  }
+
+  public static UpdateReceiptRequest ToEditRequest(this ReceiptModel receipt)
+  {
+    ArgumentNullException.ThrowIfNull(receipt);
+    return new UpdateReceiptRequest(receipt.Store, receipt.PurchaseDate);
   }
 }
