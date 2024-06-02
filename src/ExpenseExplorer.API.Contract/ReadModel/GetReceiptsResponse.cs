4a2c8dc2191caa0ab0dc43b557cd567959b224ad
frognar <sebastian.przyszlak@gmail.com>
2024-06-02 07:25:33 +0200

    Add FilteredCount to GetReceiptsResponse

--- src/ExpenseExplorer.API.Contract/ReadModel/GetReceiptsResponse.cs
+++ src/ExpenseExplorer.API.Contract/ReadModel/GetReceiptsResponse.cs
@@ -3,6 +3,7 @@ namespace ExpenseExplorer.API.Contract.ReadModel;
 public record GetReceiptsResponse(
   IEnumerable<ReceiptHeaderResponse> Receipts,
   int TotalCount,
+  int FilteredCount,
   int PageSize,
   int PageNumber,
   int PageCount);
