d546ae7162b5d29c48d7bb4b7c417495a90a2a0b
frognar <sebastian.przyszlak@gmail.com>
2024-06-08 11:10:09 +0200

    Delete run gui.sh script

--- run gui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker compose -f res/fullapi/docker-compose.yml up -d
-cd src/ExpenseExplorer.GUI/bin/publish
-dotnet ExpenseExplorer.GUI.dll
-cd ../../../..
-docker compose -f res/fullapi/docker-compose.yml stop
