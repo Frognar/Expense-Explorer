381c474032d55e3b984e62520eff45b94953985d
frognar <sebastian.przyszlak@gmail.com>
2024-06-08 11:09:29 +0200

    Publish API in build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 dotnet test --configuration Release
+dotnet publish src/ExpenseExplorer.API/ExpenseExplorer.API.csproj -p:PublishProfile=FolderProfile
 dotnet publish src/ExpenseExplorer.GUI/ExpenseExplorer.GUI.csproj -p:PublishProfile=FolderProfile
