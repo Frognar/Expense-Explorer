f79f35972d9be8a0742aa46088f362a6c8aed968
frognar <sebastian.przyszlak@gmail.com>
2024-06-09 10:50:37 +0200

    Register endpoint for getting income sources

--- src/ExpenseExplorer.API/Endpoints/SourceEndpoints.cs
+++ src/ExpenseExplorer.API/Endpoints/SourceEndpoints.cs
@@ -1,9 +1,29 @@
 namespace ExpenseExplorer.API.Endpoints;
 
+using CommandHub;
+using ExpenseExplorer.API.Mappers;
+using ExpenseExplorer.ReadModel.Queries;
+using static ExpenseExplorer.API.Endpoints.FailureHandler;
+
 public static class SourceEndpoints
 {
   public static IEndpointRouteBuilder MapSourceEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
   {
+    RouteGroupBuilder group = endpointRouteBuilder.MapGroup("/api/sources");
+    group.MapGet("/", GetSourcesAsync);
     return endpointRouteBuilder;
+  }
+
+  private static async Task<IResult> GetSourcesAsync(
+    int? pageSize,
+    int? pageNumber,
+    string? search,
+    ISender sender,
+    CancellationToken cancellationToken = default)
+  {
+    GetSourcesQuery query = new(pageSize, pageNumber, search);
+    return (await sender.SendAsync(query, cancellationToken))
+      .Map(page => page.MapToResponse())
+      .Match(Handle, Results.Ok);
   }
 }
