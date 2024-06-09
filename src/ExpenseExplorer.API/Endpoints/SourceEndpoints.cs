38068cc2ef8ab9d05b45d2f27a41961ddcbb7686
frognar <sebastian.przyszlak@gmail.com>
2024-06-09 10:41:20 +0200

    Add MapSourceEndpoints extension method

---
+++ src/ExpenseExplorer.API/Endpoints/SourceEndpoints.cs
@@ -0,0 +1,9 @@
+namespace ExpenseExplorer.API.Endpoints;
+
+public static class SourceEndpoints
+{
+  public static IEndpointRouteBuilder MapSourceEndpoints(this IEndpointRouteBuilder endpointRouteBuilder)
+  {
+    return endpointRouteBuilder;
+  }
+}
