3c246717fa1fa95d942f1c0599df965f5e27cfd7
frognar <sebastian.przyszlak@gmail.com>
2024-08-10 13:25:46 +0200

    Fail when updating deleted purchase

--- fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/Purchases/PurchaseType.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/Purchases/PurchaseType.cs
@@ -1,3 +1,4 @@
+using DotResult;
 using ExpenseExplorer.Domain.Purchases.Facts;
 using ExpenseExplorer.Domain.ValueObjects;
 
@@ -49,63 +50,85 @@ public static class Purchase
           description)));
   }
 
-  public static PurchaseType ChangeItem(
-    this PurchaseType purchase,
-    ItemType newItem)
+  public static Result<PurchaseType> ChangeItem(this PurchaseType purchase, ItemType newItem)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot change item of deleted purchase");
+    }
+
     return purchase.Item == newItem
       ? purchase
       : purchase with { Item = newItem, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseItemChanged.Create(purchase.Id, newItem)) };
   }
 
-  public static PurchaseType ChangeCategoryId(
-    this PurchaseType purchase,
-    ExpenseCategoryIdType newCategoryId)
+  public static Result<PurchaseType> ChangeCategoryId(this PurchaseType purchase, ExpenseCategoryIdType newCategoryId)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot change category of deleted purchase");
+    }
+
     return purchase.CategoryId == newCategoryId
       ? purchase
       : purchase with { CategoryId = newCategoryId, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseCategoryIdChanged.Create(purchase.Id, newCategoryId)) };
   }
 
-  public static PurchaseType ChangeQuantity(
-    this PurchaseType purchase,
-    QuantityType newQuantity)
+  public static Result<PurchaseType> ChangeQuantity(this PurchaseType purchase, QuantityType newQuantity)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot change quantity of deleted purchase");
+    }
+
     return purchase.Quantity == newQuantity
       ? purchase
       : purchase with { Quantity = newQuantity, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseQuantityChanged.Create(purchase.Id, newQuantity)) };
   }
 
-  public static PurchaseType ChangeUnitPrice(
-    this PurchaseType purchase,
-    MoneyType newUnitPrice)
+  public static Result<PurchaseType> ChangeUnitPrice(this PurchaseType purchase, MoneyType newUnitPrice)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot change unit price of deleted purchase");
+    }
+
     return purchase.UnitPrice == newUnitPrice
       ? purchase
       : purchase with { UnitPrice = newUnitPrice, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseUnitPriceChanged.Create(purchase.Id, newUnitPrice)) };
   }
 
-  public static PurchaseType ChangeTotalDiscount(
-    this PurchaseType purchase,
-    MoneyType newTotalDiscount)
+  public static Result<PurchaseType> ChangeTotalDiscount(this PurchaseType purchase, MoneyType newTotalDiscount)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot change total discount of deleted purchase");
+    }
+
     return purchase.TotalDiscount == newTotalDiscount
       ? purchase
       : purchase with { TotalDiscount = newTotalDiscount, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseTotalDiscountChanged.Create(purchase.Id, newTotalDiscount)) };
   }
 
-  public static PurchaseType ChangeDescription(
-    this PurchaseType purchase,
-    DescriptionType newDescription)
+  public static Result<PurchaseType> ChangeDescription(this PurchaseType purchase, DescriptionType newDescription)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot change description of deleted purchase");
+    }
+
     return purchase.Description == newDescription
       ? purchase
       : purchase with { Description = newDescription, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseDescriptionChanged.Create(purchase.Id, newDescription)) };
   }
 
-  public static PurchaseType Delete(
-    this PurchaseType purchase)
+  public static Result<PurchaseType> Delete(this PurchaseType purchase)
   {
+    if (purchase.Deleted)
+    {
+      return Failure.Validation(message: "Cannot delete already deleted purchase");
+    }
+
     return purchase with { Deleted = true, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseDeleted.Create(purchase.Id)) };
   }
 }
