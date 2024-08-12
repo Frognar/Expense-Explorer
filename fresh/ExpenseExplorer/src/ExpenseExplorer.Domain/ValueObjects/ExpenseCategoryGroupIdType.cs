df500487cc8b0472e5bf76f5183160feaba05d40
frognar <sebastian.przyszlak@gmail.com>
2024-08-12 09:39:03 +0200

    Add Create function to ExpenseCategoryId and ExpenseCategoryGroupId

--- fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/ValueObjects/ExpenseCategoryGroupIdType.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/ValueObjects/ExpenseCategoryGroupIdType.cs
@@ -1,3 +1,5 @@
+using DotMaybe;
+
 namespace ExpenseExplorer.Domain.ValueObjects;
 
 public readonly record struct ExpenseCategoryGroupIdType(string Value);
@@ -5,4 +7,7 @@ public readonly record struct ExpenseCategoryGroupIdType(string Value);
 public static class ExpenseCategoryGroupId
 {
   public static ExpenseCategoryGroupIdType Unique() => new(Id.Unique());
+
+  public static Maybe<ExpenseCategoryGroupIdType> Create(string value)
+    => from id in Id.Create(value) select new ExpenseCategoryGroupIdType(id);
 }
--- fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/ValueObjects/ExpenseCategoryIdType.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/ValueObjects/ExpenseCategoryIdType.cs
@@ -1,3 +1,5 @@
+using DotMaybe;
+
 namespace ExpenseExplorer.Domain.ValueObjects;
 
 public readonly record struct ExpenseCategoryIdType(string Value);
@@ -5,4 +7,7 @@ public readonly record struct ExpenseCategoryIdType(string Value);
 public static class ExpenseCategoryId
 {
   public static ExpenseCategoryIdType Unique() => new(Id.Unique());
+
+  public static Maybe<ExpenseCategoryIdType> Create(string value)
+    => from id in Id.Create(value) select new ExpenseCategoryIdType(id);
 }
