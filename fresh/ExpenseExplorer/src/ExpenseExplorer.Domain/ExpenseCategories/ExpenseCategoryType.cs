922a3ef5f811074f91ebdd7c1bb7e3fb5b42b303
Frognar <sebastian.przyszlak@gmail.com>
2024-08-13 09:58:03 +0200

    Add Recreate method for ExpenseCategory

--- fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/ExpenseCategories/ExpenseCategoryType.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/ExpenseCategories/ExpenseCategoryType.cs
@@ -1,5 +1,7 @@
+using DotMaybe;
 using DotResult;
 using ExpenseExplorer.Domain.ExpenseCategories.Facts;
+using ExpenseExplorer.Domain.Facts;
 using ExpenseExplorer.Domain.ValueObjects;
 using Version = ExpenseExplorer.Domain.ValueObjects.Version;
 
@@ -101,5 +103,77 @@ public static class ExpenseCategory
     }
 
     return category with { NumberOfUses = category.NumberOfUses - 1, UnsavedChanges = category.UnsavedChanges.Append(ExpenseCategoryUsageDecreased.Create(category.Id)) };
+  }
+
+  public static Result<ExpenseCategoryType> Recreate(IEnumerable<Fact> facts)
+  {
+    facts = facts.ToList();
+    if (facts.FirstOrDefault() is ExpenseCategoryCreated expenseCategoryCreated)
+    {
+      return facts.Skip(1)
+        .Aggregate(
+          Apply(expenseCategoryCreated),
+          (expenseCategoryGroup, fact) => expenseCategoryGroup.Bind(r => r.ApplyFact(fact)));
+    }
+
+    return Failure.Validation(message: "Invalid expenseCategory facts");
+  }
+
+  private static Result<ExpenseCategoryType> ApplyFact(
+    this ExpenseCategoryType expenseCategory,
+    Fact fact)
+  {
+    return fact switch
+    {
+      ExpenseCategoryRenamed expenseCategoryRenamed
+        => expenseCategory.Apply(expenseCategoryRenamed),
+      ExpenseCategoryDescriptionChanged expenseCategoryDescriptionChanged
+        => expenseCategory.Apply(expenseCategoryDescriptionChanged),
+      ExpenseCategoryUsageIncreased
+        => expenseCategory with { NumberOfUses = expenseCategory.NumberOfUses + 1 },
+      ExpenseCategoryUsageDecreased
+        => expenseCategory with { NumberOfUses = expenseCategory.NumberOfUses - 1 },
+      ExpenseCategoryDeleted => Failure.Validation(message: "Expense category has been deleted"),
+      _ => Failure.Validation(message: "Invalid expense category fact"),
+    };
+  }
+
+  private static Result<ExpenseCategoryType> Apply(ExpenseCategoryCreated fact)
+  {
+    Maybe<ExpenseCategoryType> expenseCategory =
+      from id in ExpenseCategoryId.Create(fact.ExpenseCategoryId)
+      from name in Name.Create(fact.Name)
+      let description = Description.Create(fact.Description)
+      select new ExpenseCategoryType(
+        id,
+        name,
+        description,
+        0,
+        false,
+        UnsavedChanges.Empty(),
+        Version.New());
+
+    return expenseCategory.Match(
+      () => Failure.Validation(message: "Failed to create expense category"),
+      Success.From);
+  }
+
+  private static Result<ExpenseCategoryType> Apply(
+    this ExpenseCategoryType expenseCategory,
+    ExpenseCategoryRenamed fact)
+  {
+    return (
+        from name in Name.Create(fact.Name)
+        select expenseCategory with { Name = name })
+      .Match(
+        () => Failure.Validation(message: "Failed to change name"),
+        Success.From);
+  }
+
+  private static Result<ExpenseCategoryType> Apply(
+    this ExpenseCategoryType expenseCategory,
+    ExpenseCategoryDescriptionChanged fact)
+  {
+    return expenseCategory with { Description = Description.Create(fact.Description) };
   }
 }
