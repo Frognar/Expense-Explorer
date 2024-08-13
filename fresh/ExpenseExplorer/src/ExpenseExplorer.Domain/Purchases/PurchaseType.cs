10f510ede49991b6ff484f4de0220847628ca43e
Frognar <sebastian.przyszlak@gmail.com>
2024-08-13 09:50:09 +0200

    Add Recreate function for Purchase

--- fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/Purchases/PurchaseType.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Domain/Purchases/PurchaseType.cs
@@ -1,12 +1,13 @@
+using DotMaybe;
 using DotResult;
+using ExpenseExplorer.Domain.Facts;
 using ExpenseExplorer.Domain.Purchases.Facts;
 using ExpenseExplorer.Domain.ValueObjects;
 using Version = ExpenseExplorer.Domain.ValueObjects.Version;
 
 namespace ExpenseExplorer.Domain.Purchases;
 
-public readonly record struct PurchaseType(
-  PurchaseIdType Id,
+public readonly record struct PurchaseType(PurchaseIdType Id,
   ReceiptIdType ReceiptId,
   ItemType Item,
   ExpenseCategoryIdType CategoryId,
@@ -53,7 +54,9 @@ public static class Purchase
       Version.New());
   }
 
-  public static Result<PurchaseType> ChangeItem(this PurchaseType purchase, ItemType newItem)
+  public static Result<PurchaseType> ChangeItem(
+    this PurchaseType purchase,
+    ItemType newItem)
   {
     if (purchase.Deleted)
     {
@@ -65,7 +68,9 @@ public static class Purchase
       : purchase with { Item = newItem, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseItemChanged.Create(purchase.Id, newItem)) };
   }
 
-  public static Result<PurchaseType> ChangeCategoryId(this PurchaseType purchase, ExpenseCategoryIdType newCategoryId)
+  public static Result<PurchaseType> ChangeCategoryId(
+    this PurchaseType purchase,
+    ExpenseCategoryIdType newCategoryId)
   {
     if (purchase.Deleted)
     {
@@ -77,7 +82,9 @@ public static class Purchase
       : purchase with { CategoryId = newCategoryId, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseCategoryIdChanged.Create(purchase.Id, newCategoryId)) };
   }
 
-  public static Result<PurchaseType> ChangeQuantity(this PurchaseType purchase, QuantityType newQuantity)
+  public static Result<PurchaseType> ChangeQuantity(
+    this PurchaseType purchase,
+    QuantityType newQuantity)
   {
     if (purchase.Deleted)
     {
@@ -89,7 +96,9 @@ public static class Purchase
       : purchase with { Quantity = newQuantity, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseQuantityChanged.Create(purchase.Id, newQuantity)) };
   }
 
-  public static Result<PurchaseType> ChangeUnitPrice(this PurchaseType purchase, MoneyType newUnitPrice)
+  public static Result<PurchaseType> ChangeUnitPrice(
+    this PurchaseType purchase,
+    MoneyType newUnitPrice)
   {
     if (purchase.Deleted)
     {
@@ -101,7 +110,9 @@ public static class Purchase
       : purchase with { UnitPrice = newUnitPrice, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseUnitPriceChanged.Create(purchase.Id, newUnitPrice)) };
   }
 
-  public static Result<PurchaseType> ChangeTotalDiscount(this PurchaseType purchase, MoneyType newTotalDiscount)
+  public static Result<PurchaseType> ChangeTotalDiscount(
+    this PurchaseType purchase,
+    MoneyType newTotalDiscount)
   {
     if (purchase.Deleted)
     {
@@ -113,7 +124,9 @@ public static class Purchase
       : purchase with { TotalDiscount = newTotalDiscount, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseTotalDiscountChanged.Create(purchase.Id, newTotalDiscount)) };
   }
 
-  public static Result<PurchaseType> ChangeDescription(this PurchaseType purchase, DescriptionType newDescription)
+  public static Result<PurchaseType> ChangeDescription(
+    this PurchaseType purchase,
+    DescriptionType newDescription)
   {
     if (purchase.Deleted)
     {
@@ -133,5 +146,140 @@ public static class Purchase
     }
 
     return purchase with { Deleted = true, UnsavedChanges = purchase.UnsavedChanges.Append(PurchaseDeleted.Create(purchase.Id)) };
+  }
+
+  public static Result<PurchaseType> Recreate(IEnumerable<Fact> facts)
+  {
+    facts = facts.ToList();
+    if (facts.FirstOrDefault() is PurchaseCreated purchaseCreated)
+    {
+      return facts.Skip(1)
+        .Aggregate(
+          Apply(purchaseCreated),
+          (purchase, fact)
+            => purchase.Bind(r => r.ApplyFact(fact)));
+    }
+
+    return Failure.Validation(message: "Invalid purchase facts");
+  }
+
+  private static Result<PurchaseType> ApplyFact(
+    this PurchaseType purchase,
+    Fact fact)
+  {
+    return fact switch
+    {
+      PurchaseItemChanged purchaseItemChanged
+        => purchase.Apply(purchaseItemChanged),
+      PurchaseCategoryIdChanged purchaseCategoryIdChanged
+        => purchase.Apply(purchaseCategoryIdChanged),
+      PurchaseQuantityChanged purchaseQuantityChanged
+        => purchase.Apply(purchaseQuantityChanged),
+      PurchaseUnitPriceChanged purchaseUnitPriceChanged
+        => purchase.Apply(purchaseUnitPriceChanged),
+      PurchaseTotalDiscountChanged purchaseTotalDiscountChanged
+        => purchase.Apply(purchaseTotalDiscountChanged),
+      PurchaseDescriptionChanged purchaseDescriptionChanged
+        => purchase.Apply(purchaseDescriptionChanged),
+      PurchaseDeleted
+        => Failure.Validation(message: "Purchase has been deleted"),
+      _ => Failure.Validation(message: "Invalid purchase fact"),
+    };
+  }
+
+  private static Result<PurchaseType> Apply(PurchaseCreated fact)
+  {
+    Maybe<PurchaseType> purchase =
+      from id in PurchaseId.Create(fact.PurchaseId)
+      from receiptId in ReceiptId.Create(fact.ReceiptId)
+      from item in Item.Create(fact.Item)
+      from categoryId in ExpenseCategoryId.Create(fact.CategoryId)
+      from quantity in Quantity.Create(fact.Quantity)
+      from unitPrice in Money.Create(fact.UnitPriceAmount, fact.UnitPriceCurrency)
+      from totalDiscount in Money.Create(fact.TotalDiscountAmount, fact.TotalDiscountCurrency)
+      let description = Description.Create(fact.Description)
+      select new PurchaseType(
+        id,
+        receiptId,
+        item,
+        categoryId,
+        quantity,
+        unitPrice,
+        totalDiscount,
+        description,
+        false,
+        UnsavedChanges.Empty(),
+        Version.New());
+
+    return purchase.Match(
+      () => Failure.Validation(message: "Failed to create purchase"),
+      Success.From);
+  }
+
+  private static Result<PurchaseType> Apply(
+    this PurchaseType purchase,
+    PurchaseItemChanged fact)
+  {
+    return (
+        from item in Item.Create(fact.Item)
+        select purchase with { Item = item })
+      .Match(
+        () => Failure.Validation(message: "Failed to change item"),
+        Success.From);
+  }
+
+  private static Result<PurchaseType> Apply(
+    this PurchaseType purchase,
+    PurchaseCategoryIdChanged fact)
+  {
+    return (
+        from categoryId in ExpenseCategoryId.Create(fact.CategoryId)
+        select purchase with { CategoryId = categoryId })
+      .Match(
+        () => Failure.Validation(message: "Failed to change category id"),
+        Success.From);
+  }
+
+  private static Result<PurchaseType> Apply(
+    this PurchaseType purchase,
+    PurchaseQuantityChanged fact)
+  {
+    return (
+        from quantity in Quantity.Create(fact.Quantity)
+        select purchase with { Quantity = quantity })
+      .Match(
+        () => Failure.Validation(message: "Failed to change quantity"),
+        Success.From);
+  }
+
+  private static Result<PurchaseType> Apply(
+    this PurchaseType purchase,
+    PurchaseUnitPriceChanged fact)
+  {
+    return (
+        from unitPrice in Money.Create(fact.Amount, fact.Currency)
+        select purchase with { UnitPrice = unitPrice })
+      .Match(
+        () => Failure.Validation(message: "Failed to change unit price"),
+        Success.From);
+  }
+
+  private static Result<PurchaseType> Apply(
+    this PurchaseType purchase,
+    PurchaseTotalDiscountChanged fact)
+  {
+    return (
+        from totalDiscount in Money.Create(fact.Amount, fact.Currency)
+        select purchase with { TotalDiscount = totalDiscount })
+      .Match(
+        () => Failure.Validation(message: "Failed to change total discount"),
+        Success.From);
+  }
+
+  private static Result<PurchaseType> Apply(
+    this PurchaseType purchase,
+    PurchaseDescriptionChanged fact)
+  {
+    return purchase with { Description = Description.Create(fact.Description) };
   }
 }
