87b82427cadd60707f29e5006a4f1d6c9ca05703
frognar <sebastian.przyszlak@gmail.com>
2024-09-12 11:39:21 +0200

    Ignore CA1032:Implement standard exception constructors

--- fresh/ExpenseExplorer/src/ExpenseExplorer.Infrastructure/Exceptions/FactReadException.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Infrastructure/Exceptions/FactReadException.cs
@@ -1,8 +1,5 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace ExpenseExplorer.Infrastructure.Exceptions;
 
-[SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "Not needed")]
 public sealed class FactReadException(
   string message,
   Exception innerException)
--- fresh/ExpenseExplorer/src/ExpenseExplorer.Infrastructure/Exceptions/FactSaveException.cs
+++ fresh/ExpenseExplorer/src/ExpenseExplorer.Infrastructure/Exceptions/FactSaveException.cs
@@ -1,8 +1,5 @@
-using System.Diagnostics.CodeAnalysis;
-
 namespace ExpenseExplorer.Infrastructure.Exceptions;
 
-[SuppressMessage("Design", "CA1032:Implement standard exception constructors", Justification = "Not needed")]
 public sealed class FactSaveException(
   string message,
   Exception innerException)
