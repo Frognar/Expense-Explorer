7fb6c5d3bcf7ba38eed97a37c926605c7885bde9
frognar <sebastian.przyszlak@gmail.com>
2025-04-23 15:54:08 +0200

    **Replace Home page with Receipts page implementation**

--- src/ExpenseExplorer.WebApp/Resources/Components/Pages/Receipts.Designer.cs
+++ src/ExpenseExplorer.WebApp/Resources/Components/Pages/Receipts.Designer.cs
@@ -14,21 +14,21 @@ namespace ExpenseExplorer.WebApp.Resources.Components.Pages {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Home {
+    internal class Receipts {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Home() {
+        internal Receipts() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("ExpenseExplorer.WebApp.Resources.Components.Pages.Home", typeof(Home).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("ExpenseExplorer.WebApp.Resources.Components.Pages.Receipts", typeof(Receipts).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -45,9 +45,9 @@ namespace ExpenseExplorer.WebApp.Resources.Components.Pages {
             }
         }
         
-        internal static string Welcome {
+        internal static string _Receipts {
             get {
-                return ResourceManager.GetString("Welcome", resourceCulture);
+                return ResourceManager.GetString("_Receipts", resourceCulture);
             }
         }
     }
