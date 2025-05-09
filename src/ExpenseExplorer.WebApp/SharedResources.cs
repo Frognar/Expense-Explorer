326945be10eb509d954e2a03ef3a1cddd05ec773
frognar <sebastian.przyszlak@gmail.com>
2025-05-09 08:29:36 +0200

    Add support for shared resources with localization data

---
+++ src/ExpenseExplorer.WebApp/SharedResources.cs
@@ -0,0 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace ExpenseExplorer.WebApp;
+
+[SuppressMessage("Maintainability", "S2094", Justification = "Shared resources")]
+internal sealed class SharedResources;
