3bab24481dc91ecd92229544b149dd8bb7c7463b
frognar <sebastian.przyszlak@gmail.com>
2025-05-20 21:20:35 +0200

    Add IReceiptRepository interface and InMemoryRepository implementation

---
+++ src/ExpenseExplorer.Application/Receipts/Data/IReceiptRepository.cs
@@ -0,0 +1,8 @@
+using ExpenseExplorer.Application.Receipts.DTO;
+
+namespace ExpenseExplorer.Application.Receipts.Data;
+
+public interface IReceiptRepository
+{
+    public Task<Unit> CreateReceipt(CreateReceiptRequest receipt);
+}
---
+++ src/ExpenseExplorer.Application/Receipts/Data/InMemoryRepository.cs
@@ -0,0 +1,15 @@
+using ExpenseExplorer.Application.Receipts.DTO;
+
+namespace ExpenseExplorer.Application.Receipts.Data;
+
+public sealed class InMemoryRepository : IReceiptRepository
+{
+    private static readonly List<ReceiptDetails> Receipts = [];
+
+    public Task<Unit> CreateReceipt(CreateReceiptRequest receipt)
+    {
+        ArgumentNullException.ThrowIfNull(receipt);
+        Receipts.Add(new ReceiptDetails(receipt.Store, receipt.PurchaseDate, []));
+        return Task.FromResult(Unit.Instance);
+    }
+}
