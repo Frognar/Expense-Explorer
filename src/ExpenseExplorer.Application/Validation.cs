b2f71ceff4074410f9a716b4940ee3eacb28d571
frognar <sebastian.przyszlak@gmail.com>
2025-05-21 23:18:05 +0200

    Add `Join` method to combine two Validated results with a custom function

--- src/ExpenseExplorer.Application/Validation.cs
+++ src/ExpenseExplorer.Application/Validation.cs
@@ -4,4 +4,21 @@ public static class Validation
 {
     public static Validated<T> Succeed<T>(T value) => Validated<T>.Succeed(value);
     public static Validated<T> Failed<T>(IEnumerable<ValidationError> errors) => Validated<T>.Failed(errors);
+
+    public static Validated<TResult> Join<T, T1, TResult>(
+        this Validated<T> source,
+        Validated<T1> other,
+        Func<T, T1, TResult> join)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        return source.Match(
+            errors: sourceErrors =>
+                other.Match(
+                    errors: otherErrors => Failed<TResult>(sourceErrors.Concat(otherErrors)),
+                    value: _ => Failed<TResult>(sourceErrors)),
+            value: sourceValue =>
+                other.Match(
+                    errors: Failed<TResult>,
+                    value: otherValue => Succeed(join(sourceValue, otherValue))));
+    }
 }
