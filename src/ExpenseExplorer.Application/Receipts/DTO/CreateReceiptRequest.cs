297494e7dc3e1fbc3ed36f732476715748ece101
frognar <sebastian.przyszlak@gmail.com>
2025-05-22 17:31:55 +0200

    Add factory delegate to CreateReceiptRequest DTO

--- src/ExpenseExplorer.Application/Receipts/DTO/CreateReceiptRequest.cs
+++ src/ExpenseExplorer.Application/Receipts/DTO/CreateReceiptRequest.cs
@@ -1,5 +1,9 @@
 using ExpenseExplorer.Application.Receipts.ValueObjects;
 
 namespace ExpenseExplorer.Application.Receipts.DTO;
+using CreateReceiptRequestFactory = Func<Store, PurchaseDate, CreateReceiptRequest>;
 
-public sealed record CreateReceiptRequest(Store Store, PurchaseDate PurchaseDate);
+public sealed record CreateReceiptRequest(Store Store, PurchaseDate PurchaseDate)
+{
+    public static readonly CreateReceiptRequestFactory Create = (s, p) => new CreateReceiptRequest(s, p);
+}
