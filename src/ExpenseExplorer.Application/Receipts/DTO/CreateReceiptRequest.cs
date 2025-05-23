de69f3c4513735f04a6b3f61de147bcf3893e1c2
frognar <sebastian.przyszlak@gmail.com>
2025-05-23 09:18:16 +0200

    Add ReceiptId to CreateReceiptRequest and update factory method

--- src/ExpenseExplorer.Application/Receipts/DTO/CreateReceiptRequest.cs
+++ src/ExpenseExplorer.Application/Receipts/DTO/CreateReceiptRequest.cs
@@ -1,9 +1,11 @@
 using ExpenseExplorer.Application.Receipts.ValueObjects;
 
 namespace ExpenseExplorer.Application.Receipts.DTO;
+
 using CreateReceiptRequestFactory = Func<Store, PurchaseDate, CreateReceiptRequest>;
 
-public sealed record CreateReceiptRequest(Store Store, PurchaseDate PurchaseDate)
+public sealed record CreateReceiptRequest(ReceiptId Id, Store Store, PurchaseDate PurchaseDate)
 {
-    public static readonly CreateReceiptRequestFactory Create = (s, p) => new CreateReceiptRequest(s, p);
+    public static readonly CreateReceiptRequestFactory Create = (s, p) =>
+        new CreateReceiptRequest(ReceiptId.Unique(), s, p);
 }
