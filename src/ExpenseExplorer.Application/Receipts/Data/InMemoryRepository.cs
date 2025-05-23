efb7939b40eba98bb03c8ba9c6b8d2d333275cf5
frognar <sebastian.przyszlak@gmail.com>
2025-05-23 09:26:36 +0200

    Make InMemoryRepository internal and remove redundant null check

--- src/ExpenseExplorer.Application/Receipts/Data/InMemoryRepository.cs
+++ src/ExpenseExplorer.Application/Receipts/Data/InMemoryRepository.cs
@@ -2,13 +2,12 @@ using ExpenseExplorer.Application.Receipts.DTO;
 
 namespace ExpenseExplorer.Application.Receipts.Data;
 
-public sealed class InMemoryRepository : IReceiptRepository
+internal sealed class InMemoryRepository : IReceiptRepository
 {
     private static readonly List<ReceiptDetails> Receipts = [];
 
     public Task<Unit> CreateReceipt(CreateReceiptRequest receipt, CancellationToken cancellationToken)
     {
-        ArgumentNullException.ThrowIfNull(receipt);
         Receipts.Add(new ReceiptDetails(receipt.Store, receipt.PurchaseDate, []));
         return Task.FromResult(Unit.Instance);
     }
