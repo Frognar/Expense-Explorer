94e2e82fc694da611db7ebaff2679a31a4da14ce
frognar <sebastian.przyszlak@gmail.com>
2025-05-24 22:23:17 +0200

    Add arithmetic operations and Zero constant to Money value object

--- src/ExpenseExplorer.Application/Receipts/ValueObjects/Money.cs
+++ src/ExpenseExplorer.Application/Receipts/ValueObjects/Money.cs
@@ -12,4 +12,19 @@ public readonly record struct Money
             ? Result.Failure<Money, string>("Money cannot be negative")
             : Result.Success<Money, string>(new Money(value));
     }
+
+    public static readonly Money Zero = new(0);
+
+    public static Money operator +(Money left, Money right) => Add(left, right);
+
+    public static Money Add(Money left, Money right) => new(left.Value + right.Value);
+
+    public static Money operator *(Money left, Quantity right) => Multiply(left, right);
+
+    public static Money Multiply(Money left, Quantity right) => new(left.Value * right.Value);
+
+    public static Money operator -(Money left, Money right) => Subtract(left, right);
+
+    public static Money Subtract(Money left, Money right) =>
+        left.Value < right.Value ? Zero : new Money(left.Value - right.Value);
 }
