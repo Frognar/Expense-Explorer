a4a1896b9e022d8224800e2bb56592be2af132a5
frognar <sebastian.przyszlak@gmail.com>
2025-05-25 11:11:35 +0200

    Add filtering, pagination, and total cost retrieval to receipt repository

--- src/ExpenseExplorer.Application/Receipts/Data/IReceiptRepository.cs
+++ src/ExpenseExplorer.Application/Receipts/Data/IReceiptRepository.cs
@@ -1,8 +1,21 @@
+using System.Collections.Immutable;
 using ExpenseExplorer.Application.Receipts.DTO;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
 
 namespace ExpenseExplorer.Application.Receipts.Data;
 
 public interface IReceiptRepository
 {
     public Task<Unit> CreateReceipt(CreateReceiptRequest receipt, CancellationToken cancellationToken);
+
+    public Task<PageOf<ReceiptSummary>> GetReceiptsAsync(
+        int pageSize,
+        int skip,
+        ReceiptOrder order,
+        ImmutableArray<ReceiptFilter> filters,
+        CancellationToken cancellationToken);
+
+    public Task<Money> GetTotalCostAsync(
+        ImmutableArray<ReceiptFilter> filters,
+        CancellationToken cancellationToken);
 }
--- src/ExpenseExplorer.Application/Receipts/Data/InMemoryRepository.cs
+++ src/ExpenseExplorer.Application/Receipts/Data/InMemoryRepository.cs
@@ -1,4 +1,6 @@
+using System.Collections.Immutable;
 using ExpenseExplorer.Application.Receipts.DTO;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
 
 namespace ExpenseExplorer.Application.Receipts.Data;
 
@@ -10,5 +12,66 @@ internal sealed class InMemoryRepository : IReceiptRepository
     {
         Receipts.Add(new ReceiptDetails(receipt.Id, receipt.Store, receipt.PurchaseDate, []));
         return Task.FromResult(Unit.Instance);
+    }
+
+    public async Task<PageOf<ReceiptSummary>> GetReceiptsAsync(
+        int pageSize,
+        int skip,
+        ReceiptOrder order,
+        ImmutableArray<ReceiptFilter> filters,
+        CancellationToken cancellationToken)
+    {
+        IEnumerable<ReceiptSummary> receipts = GetReceipts(filters);
+        int count = receipts.Count();
+        receipts = order.Descending
+            ? receipts.OrderByDescending(GetOrderBy(order.OrderBy))
+            : receipts.OrderBy(GetOrderBy(order.OrderBy));
+
+        List<ReceiptSummary> r = receipts.Skip(skip).Take(pageSize).ToList();
+        await Task.CompletedTask;
+        return Page.Of([..r], (uint)count);
+    }
+
+    private static IEnumerable<ReceiptSummary> GetReceipts(
+        ImmutableArray<ReceiptFilter> filters)
+    {
+        return Receipts
+            .Select(r => new ReceiptSummary(
+                r.Id,
+                r.Store,
+                r.PurchaseDate,
+                r.Items.Aggregate(
+                    Money.Zero, (m, i) => m + (i.UnitPrice * i.Quantity - i.Discount ?? Money.Zero))))
+            .Where(MatchesReceiptFilters(filters));
+    }
+
+    private static Func<ReceiptSummary, bool> MatchesReceiptFilters(ImmutableArray<ReceiptFilter> filters) =>
+        r => filters.All(f => MatchesReceiptFilter(r, f));
+
+    private static bool MatchesReceiptFilter(ReceiptSummary receipt, ReceiptFilter filter) =>
+        (!filter.Stores.Any() || filter.Stores.Contains(receipt.Store.Name))
+        && (filter.PurchaseDateFrom == null || receipt.PurchaseDate.Date >= filter.PurchaseDateFrom)
+        && (filter.PurchaseDateTo == null || receipt.PurchaseDate.Date <= filter.PurchaseDateTo)
+        && (filter.TotalCostMin == null || receipt.Total.Value >= filter.TotalCostMin)
+        && (filter.TotalCostMax == null || receipt.Total.Value <= filter.TotalCostMax);
+
+    private static Func<ReceiptSummary, object> GetOrderBy(string orderBy)
+    {
+        return orderBy switch
+        {
+            "Store" => r => r.Store.Name,
+            "PurchaseDate" => r => r.PurchaseDate.Date,
+            "TotalCost" => r => r.Total.Value,
+            _ => r => r.Id.Value
+        };
+    }
+
+    public async Task<Money> GetTotalCostAsync(
+        ImmutableArray<ReceiptFilter> filters,
+        CancellationToken cancellationToken)
+    {
+        IEnumerable<ReceiptSummary> receipts = GetReceipts(filters);
+        await Task.CompletedTask;
+        return receipts.Aggregate(Money.Zero, (m, r) => m + r.Total);
     }
 }
