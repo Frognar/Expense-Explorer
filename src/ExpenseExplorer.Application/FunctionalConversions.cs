24a773bab3d1f18a7384aada3112371693a5b23d
frognar <sebastian.przyszlak@gmail.com>
2025-05-27 22:58:11 +0200

    Simplify `ToValidated` and `ToResult` methods by converting to expression-bodied members.

--- src/ExpenseExplorer.Application/FunctionalConversions.cs
+++ src/ExpenseExplorer.Application/FunctionalConversions.cs
@@ -2,19 +2,13 @@ namespace ExpenseExplorer.Application;
 
 public static class FunctionalConversions
 {
-    public static Validated<T> ToValidated<T>(this Option<T> source, Func<ValidationError> onNone)
-    {
-        ArgumentNullException.ThrowIfNull(source);
-        return source.Match(
+    public static Validated<T> ToValidated<T>(this Option<T> source, Func<ValidationError> onNone) =>
+        source.Match(
             none: () => Validation.Failed<T>([onNone()]),
             some: Validation.Succeed);
-    }
 
-    public static Result<T, IEnumerable<ValidationError>> ToResult<T>(this Validated<T> source)
-    {
-        ArgumentNullException.ThrowIfNull(source);
-        return source.Match(
+    public static Result<T, IEnumerable<ValidationError>> ToResult<T>(this Validated<T> source) =>
+        source.Match(
             errors: Result.Failure<T, IEnumerable<ValidationError>>,
             value: Result.Success<T, IEnumerable<ValidationError>>);
-    }
 }
