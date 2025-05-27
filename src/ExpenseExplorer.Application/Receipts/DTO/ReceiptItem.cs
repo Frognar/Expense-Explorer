cb5367deec3768e9b008ce6f6e1e9ea661b37687
frognar <sebastian.przyszlak@gmail.com>
2025-05-27 23:16:25 +0200

    Add Id property to ReceiptItem DTO

--- src/ExpenseExplorer.Application/Receipts/DTO/ReceiptItem.cs
+++ src/ExpenseExplorer.Application/Receipts/DTO/ReceiptItem.cs
@@ -3,6 +3,7 @@ using ExpenseExplorer.Application.Receipts.ValueObjects;
 namespace ExpenseExplorer.Application.Receipts.DTO;
 
 public sealed record ReceiptItem(
+    ReceiptItemId Id,
     Item Item,
     Category Category,
     Money UnitPrice,
