0c3bbdea4728def200ebb770e7dc674a06840417
frognar <sebastian.przyszlak@gmail.com>
2025-05-27 22:56:12 +0200

    Refactor ReceiptId.TryCreate to accept Guid instead of string.

--- src/ExpenseExplorer.Application/Receipts/ValueObjects/ReceiptId.cs
+++ src/ExpenseExplorer.Application/Receipts/ValueObjects/ReceiptId.cs
@@ -7,8 +7,8 @@ public readonly record struct ReceiptId
     private ReceiptId(Guid value) => Value = value;
 
     public static ReceiptId Unique() => new(Guid.CreateVersion7());
-    public static Option<ReceiptId> TryCreate(string value) =>
-        Guid.TryParse(value, out Guid guid)
-            ? Option.Some(new ReceiptId(guid))
+    public static Option<ReceiptId> TryCreate(Guid value) =>
+        value != Guid.Empty
+            ? Option.Some(new ReceiptId(value))
             : Option.None<ReceiptId>();
 }
