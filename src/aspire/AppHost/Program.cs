52afb2050ec079c3b10e80df85667cf179ed1a71
frognar <sebastian.przyszlak@gmail.com>
2025-05-31 06:18:45 +0200

    Integrate ExpenseExplorer.WebApp project into AppHost and configure PostgreSQL resources

--- src/aspire/AppHost/Program.cs
+++ src/aspire/AppHost/Program.cs
@@ -1,3 +1,16 @@
+using AppHost;
+
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+IResourceBuilder<PostgresServerResource> postgres = builder
+    .AddPostgres("postgres")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+IResourceBuilder<PostgresDatabaseResource> db = postgres
+    .CreateDatabase("expense-explorer");
+
+builder.AddProject<Projects.ExpenseExplorer_WebApp>("expense-explorer-web-app")
+    .WithReference(db)
+    .WaitFor(db);
 
 await builder.Build().RunAsync();
