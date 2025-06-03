fe3995278dbbdd7a8f01eec9fbaac15a887f3542
frognar <sebastian.przyszlak@gmail.com>
2025-06-03 07:10:50 +0200

    Replace Result with Option in ReceiptItemId's TryCreate method

--- src/ExpenseExplorer.Application/Receipts/ValueObjects/ReceiptItemId.cs
+++ src/ExpenseExplorer.Application/Receipts/ValueObjects/ReceiptItemId.cs
@@ -7,4 +7,8 @@ public readonly record struct ReceiptItemId
     private ReceiptItemId(Guid value) => Value = value;
 
     public static ReceiptItemId Unique() => new(Guid.CreateVersion7());
+    public static Option<ReceiptItemId> TryCreate(Guid value) =>
+        value != Guid.Empty
+            ? Option.Some(new ReceiptItemId(value))
+            : Option.None<ReceiptItemId>();
 }
