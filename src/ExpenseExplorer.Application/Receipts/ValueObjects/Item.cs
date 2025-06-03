8320a8dffb4903c1c136b085d76d878b80da9850
frognar <sebastian.przyszlak@gmail.com>
2025-06-03 07:08:31 +0200

    Add Items static class with CreateMany method for multiple Item instances

--- src/ExpenseExplorer.Application/Receipts/ValueObjects/Item.cs
+++ src/ExpenseExplorer.Application/Receipts/ValueObjects/Item.cs
@@ -1,3 +1,6 @@
+using System.Collections.Immutable;
+using System.Diagnostics;
+
 namespace ExpenseExplorer.Application.Receipts.ValueObjects;
 
 public readonly record struct Item
@@ -12,4 +15,14 @@ public readonly record struct Item
             ? Option.None<Item>()
             : Option.Some(new Item(name.Trim()));
     }
+}
+
+public static class Items
+{
+    public static Option<ImmutableArray<Item>> CreateMany(params IEnumerable<Option<Item>> items) =>
+        items.Any(o => o.IsNone)
+            ? Option.None<ImmutableArray<Item>>()
+            : Option.Some<ImmutableArray<Item>>([
+                ..items.Select(o => o.OrElse(() => throw new UnreachableException()))
+            ]);
 }
