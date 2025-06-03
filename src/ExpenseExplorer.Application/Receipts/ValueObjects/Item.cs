46d50c28a07bbd9ac432e05226d794e1a5e39664
frognar <sebastian.przyszlak@gmail.com>
2025-06-03 07:07:27 +0200

    Replace Result with Option in Item's TryCreate method

--- src/ExpenseExplorer.Application/Receipts/ValueObjects/Item.cs
+++ src/ExpenseExplorer.Application/Receipts/ValueObjects/Item.cs
@@ -3,13 +3,13 @@ namespace ExpenseExplorer.Application.Receipts.ValueObjects;
 public readonly record struct Item
 {
     public string Name { get; }
-    
+
     private Item(string name) => Name = name;
 
-    public static Result<Item, string> TryCreate(string name)
+    public static Option<Item> TryCreate(string name)
     {
         return string.IsNullOrWhiteSpace(name)
-            ? Result.Failure<Item, string>("Item name cannot be empty")
-            : Result.Success<Item, string>(new Item(name.Trim()));
+            ? Option.None<Item>()
+            : Option.Some(new Item(name.Trim()));
     }
 }
