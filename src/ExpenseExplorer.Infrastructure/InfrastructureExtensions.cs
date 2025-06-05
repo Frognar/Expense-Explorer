cba7e651e9e64cdf1f4175118d652ca115f898ed
frognar <sebastian.przyszlak@gmail.com>
2025-06-05 06:27:15 +0200

    Add InfrastructureExtensions for services and database setup

---
+++ src/ExpenseExplorer.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,29 @@
+using ExpenseExplorer.Application.Receipts.Data;
+using ExpenseExplorer.Infrastructure.Data;
+using ExpenseExplorer.Infrastructure.Database;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpenseExplorer.Infrastructure;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        return services
+            .AddScoped<IStoreRepository, Repository>();
+    }
+
+    public static IServiceCollection AddDatabase(
+        this IServiceCollection services,
+        string connectionString)
+    {
+        return services
+            .AddSingleton<IDbConnectionFactory>(_ => new DbConnectionFactory(connectionString))
+            .AddSingleton<DbInitializer>();
+    }
+
+    public static async Task InitializeDatabase(this IServiceProvider services)
+    {
+        await services.GetRequiredService<DbInitializer>().InitializeAsync();
+    }
+}
