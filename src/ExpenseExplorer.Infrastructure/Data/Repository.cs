5e8bd5038e35b577c2165682212fa7204bd661d2
frognar <sebastian.przyszlak@gmail.com>
2025-06-05 06:27:03 +0200

    Add Repository implementation for store retrieval with optional search filters

---
+++ src/ExpenseExplorer.Infrastructure/Data/Repository.cs
@@ -0,0 +1,42 @@
+using System.Collections.Immutable;
+using System.Data;
+using Dapper;
+using ExpenseExplorer.Application;
+using ExpenseExplorer.Application.Receipts.Data;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
+using ExpenseExplorer.Infrastructure.Database;
+
+namespace ExpenseExplorer.Infrastructure.Data;
+
+internal sealed class Repository(IDbConnectionFactory connectionFactory)
+    : IStoreRepository
+{
+    public async Task<ImmutableArray<Store>> GetStoresAsync(Option<string> search, CancellationToken cancellationToken)
+    {
+        using IDbConnection connection = await connectionFactory.CreateConnectionAsync(CancellationToken.None);
+        IEnumerable<string> stores = await search
+            .Map(FormatStoreSearchFilters)
+            .MatchAsync(
+                none: () => connection.QueryAsync<string>("""
+                                                          select store
+                                                          from receipts
+                                                          """),
+                some: whereClauses => connection.QueryAsync<string>($"""
+                                                                     select store
+                                                                     from receipts
+                                                                     where {whereClauses}
+                                                                     """
+                ));
+
+        return Stores
+            .CreateMany([..stores.Select(Store.TryCreate)])
+            .OrElse(() => []);
+    }
+
+    private static string FormatStoreSearchFilters(string searchTerms)
+    {
+        return string.Join(" AND ", searchTerms
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(str => $"UPPER(store) LIKE '%{str.ToUpperInvariant()}%'"));
+    }
+}
