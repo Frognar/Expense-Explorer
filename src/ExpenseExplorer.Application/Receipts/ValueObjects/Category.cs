94eff8e89a2f57130981466df59ad9c1bdca62c8
frognar <sebastian.przyszlak@gmail.com>
2025-06-08 07:21:00 +0200

    Remove unused `Categories` class and redundant usings from `Category` value object.

--- src/ExpenseExplorer.Application/Receipts/ValueObjects/Category.cs
+++ src/ExpenseExplorer.Application/Receipts/ValueObjects/Category.cs
@@ -1,6 +1,3 @@
-using System.Collections.Immutable;
-using System.Diagnostics;
-
 namespace ExpenseExplorer.Application.Receipts.ValueObjects;
 
 public readonly record struct Category
@@ -13,14 +10,4 @@ public readonly record struct Category
         string.IsNullOrWhiteSpace(name)
             ? Option.None<Category>()
             : Option.Some(new Category(name.Trim()));
-}
-
-public static class Categories
-{
-    public static Option<ImmutableArray<Category>> CreateMany(params IEnumerable<Option<Category>> categories) =>
-        categories.Any(o => o.IsNone)
-            ? Option.None<ImmutableArray<Category>>()
-            : Option.Some<ImmutableArray<Category>>([
-                ..categories.Select(o => o.OrElse(() => throw new UnreachableException()))
-            ]);
 }
