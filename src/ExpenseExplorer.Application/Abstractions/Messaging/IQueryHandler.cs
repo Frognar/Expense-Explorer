b5d212f27b84057be72bcbd9f0d1ef6b0d97375a
frognar <sebastian.przyszlak@gmail.com>
2025-06-14 12:09:36 +0200

    Update `IQueryHandler` to return `Result<TResponse>` using `DotResult`

--- src/ExpenseExplorer.Application/Abstractions/Messaging/IQueryHandler.cs
+++ src/ExpenseExplorer.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -1,6 +1,8 @@
+using DotResult;
+
 namespace ExpenseExplorer.Application.Abstractions.Messaging;
 
 public interface IQueryHandler<in TQuery, TResponse> where TQuery : IQuery<TResponse>
 {
-    Task<TResponse> HandleAsync(TQuery query, CancellationToken cancellationToken);
+    Task<Result<TResponse>> HandleAsync(TQuery query, CancellationToken cancellationToken);
 }
