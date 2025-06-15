8c97c02b1bf01ceea4dedee3bf731df1092976a2
frognar <sebastian.przyszlak@gmail.com>
2025-06-15 07:52:36 +0200

    Add `Apply` method overloads to `Validation` class for streamlined functional chaining

--- src/ExpenseExplorer.Application/Validation.cs
+++ src/ExpenseExplorer.Application/Validation.cs
@@ -5,6 +5,86 @@ public static class Validation
     public static Validated<T> Succeed<T>(T value) => Validated<T>.Succeed(value);
     public static Validated<T> Failed<T>(IEnumerable<ValidationError> errors) => Validated<T>.Failed(errors);
 
+    public static Validated<Func<T2, T3, T4, T5, T6, T7, TResult>> Apply<T1, T2, T3, T4, T5, T6, T7, TResult>(
+        this Func<T1, T2, T3, T4, T5, T6, T7, TResult> map,
+        Validated<T1> value) =>
+        Succeed(map).Apply(value);
+
+    public static Validated<Func<T2, T3, T4, T5, T6, T7, TResult>> Apply<T1, T2, T3, T4, T5, T6, T7, TResult>(
+        this Validated<Func<T1, T2, T3, T4, T5, T6, T7, TResult>> map,
+        Validated<T1> value) =>
+        map.Match(
+            errors: errors => value.Match(
+                errors: otherErrors => Failed<Func<T2, T3, T4, T5, T6, T7, TResult>>(errors.Concat(otherErrors)),
+                value: _ => Failed<Func<T2, T3, T4, T5, T6, T7, TResult>>(errors)),
+            value: f => value.Match(
+                errors: Failed<Func<T2, T3, T4, T5, T6, T7, TResult>>,
+                value: t1 => Succeed((T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7) => f(t1, t2, t3, t4, t5, t6, t7))));
+
+    public static Validated<Func<T2, T3, T4, T5, T6, TResult>> Apply<T1, T2, T3, T4, T5, T6, TResult>(
+        this Func<T1, T2, T3, T4, T5, T6, TResult> map,
+        Validated<T1> value) =>
+        Succeed(map).Apply(value);
+
+    public static Validated<Func<T2, T3, T4, T5, T6, TResult>> Apply<T1, T2, T3, T4, T5, T6, TResult>(
+        this Validated<Func<T1, T2, T3, T4, T5, T6, TResult>> map,
+        Validated<T1> value) =>
+        map.Match(
+            errors: errors => value.Match(
+                errors: otherErrors => Failed<Func<T2, T3, T4, T5, T6, TResult>>(errors.Concat(otherErrors)),
+                value: _ => Failed<Func<T2, T3, T4, T5, T6, TResult>>(errors)),
+            value: f => value.Match(
+                errors: Failed<Func<T2, T3, T4, T5, T6, TResult>>,
+                value: t1 => Succeed((T2 t2, T3 t3, T4 t4, T5 t5, T6 t6) => f(t1, t2, t3, t4, t5, t6))));
+
+    public static Validated<Func<T2, T3, T4, T5, TResult>> Apply<T1, T2, T3, T4, T5, TResult>(
+        this Func<T1, T2, T3, T4, T5, TResult> map,
+        Validated<T1> value) =>
+        Succeed(map).Apply(value);
+
+    public static Validated<Func<T2, T3, T4, T5, TResult>> Apply<T1, T2, T3, T4, T5, TResult>(
+        this Validated<Func<T1, T2, T3, T4, T5, TResult>> map,
+        Validated<T1> value) =>
+        map.Match(
+            errors: errors => value.Match(
+                errors: otherErrors => Failed<Func<T2, T3, T4, T5, TResult>>(errors.Concat(otherErrors)),
+                value: _ => Failed<Func<T2, T3, T4, T5, TResult>>(errors)),
+            value: f => value.Match(
+                errors: Failed<Func<T2, T3, T4, T5, TResult>>,
+                value: t1 => Succeed((T2 t2, T3 t3, T4 t4, T5 t5) => f(t1, t2, t3, t4, t5))));
+
+    public static Validated<Func<T2, T3, T4, TResult>> Apply<T1, T2, T3, T4, TResult>(
+        this Func<T1, T2, T3, T4, TResult> map,
+        Validated<T1> value) =>
+        Succeed(map).Apply(value);
+
+    public static Validated<Func<T2, T3, T4, TResult>> Apply<T1, T2, T3, T4, TResult>(
+        this Validated<Func<T1, T2, T3, T4, TResult>> map,
+        Validated<T1> value) =>
+        map.Match(
+            errors: errors => value.Match(
+                errors: otherErrors => Failed<Func<T2, T3, T4, TResult>>(errors.Concat(otherErrors)),
+                value: _ => Failed<Func<T2, T3, T4, TResult>>(errors)),
+            value: f => value.Match(
+                errors: Failed<Func<T2, T3, T4, TResult>>,
+                value: t1 => Succeed((T2 t2, T3 t3, T4 t4) => f(t1, t2, t3, t4))));
+
+    public static Validated<Func<T2, T3, TResult>> Apply<T1, T2, T3, TResult>(
+        this Func<T1, T2, T3, TResult> map,
+        Validated<T1> value) =>
+        Succeed(map).Apply(value);
+
+    public static Validated<Func<T2, T3, TResult>> Apply<T1, T2, T3, TResult>(
+        this Validated<Func<T1, T2, T3, TResult>> map,
+        Validated<T1> value) =>
+        map.Match(
+            errors: errors => value.Match(
+                errors: otherErrors => Failed<Func<T2, T3, TResult>>(errors.Concat(otherErrors)),
+                value: _ => Failed<Func<T2, T3, TResult>>(errors)),
+            value: f => value.Match(
+                errors: Failed<Func<T2, T3, TResult>>,
+                value: t1 => Succeed((T2 t2, T3 t3) => f(t1, t2, t3))));
+
     public static Validated<Func<T2, TResult>> Apply<T1, T2, TResult>(
         this Func<T1, T2, TResult> map,
         Validated<T1> value) =>
@@ -31,5 +111,4 @@ public static class Validation
             value: f => value.Match(
                 errors: Failed<TResult>,
                 value: t1 => Succeed(f(t1))));
-
 }
