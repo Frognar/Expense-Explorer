53e2e5086a4f37ae517e6a745577828d83aba069
frognar <sebastian.przyszlak@gmail.com>
2025-06-15 07:48:01 +0200

    Remove `Join` method from `Validation` class to simplify validation functionality

--- src/ExpenseExplorer.Application/Validation.cs
+++ src/ExpenseExplorer.Application/Validation.cs
@@ -5,23 +5,6 @@ public static class Validation
     public static Validated<T> Succeed<T>(T value) => Validated<T>.Succeed(value);
     public static Validated<T> Failed<T>(IEnumerable<ValidationError> errors) => Validated<T>.Failed(errors);
 
-    public static Validated<TResult> Join<T, T1, TResult>(
-        this Validated<T> source,
-        Validated<T1> other,
-        Func<T, T1, TResult> join)
-    {
-        ArgumentNullException.ThrowIfNull(source);
-        return source.Match(
-            errors: sourceErrors =>
-                other.Match(
-                    errors: otherErrors => Failed<TResult>(sourceErrors.Concat(otherErrors)),
-                    value: _ => Failed<TResult>(sourceErrors)),
-            value: sourceValue =>
-                other.Match(
-                    errors: Failed<TResult>,
-                    value: otherValue => Succeed(join(sourceValue, otherValue))));
-    }
-
     public static Validated<Func<T2, TResult>> Apply<T1, T2, TResult>(
         this Func<T1, T2, TResult> map,
         Validated<T1> value) =>
