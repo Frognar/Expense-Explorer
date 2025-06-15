2698ec7c6061f78e0aa0f0d87ec9d2b739ffd64f
frognar <sebastian.przyszlak@gmail.com>
2025-06-15 07:53:18 +0200

    Introduce `AddReceiptItemCommand` record and factory for adding receipt item functionality

---
+++ src/ExpenseExplorer.Application/Features/Receipts/AddItem/AddReceiptItemCommand.cs
@@ -0,0 +1,22 @@
+using DotMaybe;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
+
+namespace ExpenseExplorer.Application.Features.Receipts.AddItem;
+
+public sealed record AddReceiptItemCommand(
+    ReceiptItemId Id,
+    ReceiptId ReceiptId,
+    Item Item,
+    Category Category,
+    Quantity Quantity,
+    Money UnitPrice,
+    Maybe<Money> Discount,
+    Maybe<Description> Description)
+    : ICommand<AddReceiptItemResponse>;
+
+public static class AddReceiptItemCommandFactory
+{
+    public static Func<ReceiptId, Item, Category, Quantity, Money, Maybe<Money>, Maybe<Description>, AddReceiptItemCommand> Create =>
+        (rId, i, c, q, up, d, de) => new AddReceiptItemCommand(ReceiptItemId.Unique(), rId, i, c, q, up, d, de);
+}
