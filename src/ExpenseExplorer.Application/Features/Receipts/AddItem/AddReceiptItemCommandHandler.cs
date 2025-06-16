99d32104bbcd7661a62cdf95067c5a4a220a548d
frognar <sebastian.przyszlak@gmail.com>
2025-06-16 07:14:33 +0200

    Introduce `AddReceiptItemCommandHandler` for handling add receipt item commands

---
+++ src/ExpenseExplorer.Application/Features/Receipts/AddItem/AddReceiptItemCommandHandler.cs
@@ -0,0 +1,30 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using ExpenseExplorer.Application.Domain.Receipts;
+
+namespace ExpenseExplorer.Application.Features.Receipts.AddItem;
+
+public class AddReceiptItemCommandHandler(
+    IAddReceiptItemPersistence persistence)
+    : ICommandHandler<AddReceiptItemCommand, AddReceiptItemResponse>
+{
+    public async Task<Result<AddReceiptItemResponse>> HandleAsync(AddReceiptItemCommand command, CancellationToken cancellationToken)
+    {
+        return await persistence.GetReceiptByIdAsync(command.ReceiptId, cancellationToken)
+                .BindAsync(r => AddItemToReceipt(r, command))
+                .BindAsync(async r => await persistence.SaveReceiptAsync(r, cancellationToken))
+                .MapAsync(_ => new AddReceiptItemResponse(command.Id.Value));
+    }
+
+    private static Result<Receipt> AddItemToReceipt(Receipt receipt, AddReceiptItemCommand command)
+    {
+        return receipt.AddItem(
+            command.Id,
+            command.Item,
+            command.Category,
+            command.Quantity,
+            command.UnitPrice,
+            command.Discount,
+            command.Description);
+    }
+}
