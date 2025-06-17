53b66c0130167d5f0cb519b5dd5e994d0b26930f
frognar <sebastian.przyszlak@gmail.com>
2025-06-17 07:29:12 +0200

    Introduce `DeleteReceiptItemCommandValidator` for validating delete receipt item requests

---
+++ src/ExpenseExplorer.Application/Features/Receipts/DeleteItem/DeleteReceiptItemCommandValidator.cs
@@ -0,0 +1,33 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
+
+namespace ExpenseExplorer.Application.Features.Receipts.DeleteItem;
+
+public sealed class DeleteReceiptItemCommandValidator(
+    ICommandHandler<DeleteReceiptItemCommand, DeleteReceiptItemResponse> inner)
+    : ICommandHandler<DeleteReceiptItemRequest, DeleteReceiptItemResponse>
+{
+    public async Task<Result<DeleteReceiptItemResponse>> HandleAsync(
+        DeleteReceiptItemRequest command,
+        CancellationToken cancellationToken)
+    {
+        return await DeleteReceiptItemCommandFactory.Create
+            .Apply(ValidateReceiptId(command.ReceiptId))
+            .Apply(ValidateReceiptItemId(command.ReceiptItemId))
+            .ToResult()
+            .BindAsync(cmd => inner.HandleAsync(cmd, cancellationToken));
+    }
+
+    private static Validated<ReceiptId> ValidateReceiptId(Guid receiptId)
+    {
+        return ReceiptId.TryCreate(receiptId)
+            .ToValidated(() => new ValidationError(nameof(receiptId), "Receipt ID cannot be empty"));
+    }
+
+    private static Validated<ReceiptItemId> ValidateReceiptItemId(Guid receiptItemId)
+    {
+        return ReceiptItemId.TryCreate(receiptItemId)
+            .ToValidated(() => new ValidationError(nameof(receiptItemId), "Receipt Item ID cannot be empty"));
+    }
+}
