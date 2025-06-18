54250a99401a126e2f30dc3200b64fc91f9e8c98
frognar <sebastian.przyszlak@gmail.com>
2025-06-18 07:17:49 +0200

    Introduce `UpdateReceiptItemCommandHandler` for handling update receipt item commands

---
+++ src/ExpenseExplorer.Application/Features/Receipts/UpdateItem/AddReceiptItemCommandHandler.cs
@@ -0,0 +1,32 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using ExpenseExplorer.Application.Domain.Receipts;
+
+namespace ExpenseExplorer.Application.Features.Receipts.UpdateItem;
+
+public class UpdateReceiptItemCommandHandler(
+    IUpdateReceiptItemPersistence persistence)
+    : ICommandHandler<UpdateReceiptItemCommand, UpdateReceiptItemResponse>
+{
+    public async Task<Result<UpdateReceiptItemResponse>> HandleAsync(UpdateReceiptItemCommand command,
+        CancellationToken cancellationToken)
+    {
+        return await persistence.GetReceiptByIdAsync(command.ReceiptId, cancellationToken)
+            .BindAsync(r => UpdateItemToReceipt(r, command))
+            .BindAsync(async r => await persistence.SaveReceiptAsync(r, cancellationToken))
+            .MapAsync(_ => new UpdateReceiptItemResponse());
+    }
+
+    private static Result<Receipt> UpdateItemToReceipt(Receipt receipt, UpdateReceiptItemCommand command)
+    {
+        return receipt.UpdateItem(new ReceiptItem(
+            command.ReceiptItemId,
+            command.ReceiptId,
+            command.Item,
+            command.Category,
+            command.Quantity,
+            command.UnitPrice,
+            command.Discount,
+            command.Description));
+    }
+}
