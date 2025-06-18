df18b7d4a7840662abc1f42ade2e47a79cacc534
frognar <sebastian.przyszlak@gmail.com>
2025-06-18 07:17:00 +0200

    Add `Apply` extension method to support validation with 8 parameters

--- src/ExpenseExplorer.Application/Validation.cs
+++ src/ExpenseExplorer.Application/Validation.cs
@@ -5,6 +5,22 @@ public static class Validation
     public static Validated<T> Succeed<T>(T value) => Validated<T>.Succeed(value);
     public static Validated<T> Failed<T>(IEnumerable<ValidationError> errors) => Validated<T>.Failed(errors);
 
+    public static Validated<Func<T2, T3, T4, T5, T6, T7, T8, TResult>> Apply<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
+        this Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult> map,
+        Validated<T1> value) =>
+        Succeed(map).Apply(value);
+
+    public static Validated<Func<T2, T3, T4, T5, T6, T7, T8, TResult>> Apply<T1, T2, T3, T4, T5, T6, T7, T8, TResult>(
+        this Validated<Func<T1, T2, T3, T4, T5, T6, T7, T8, TResult>> map,
+        Validated<T1> value) =>
+        map.Match(
+            errors: errors => value.Match(
+                errors: otherErrors => Failed<Func<T2, T3, T4, T5, T6, T7, T8, TResult>>(errors.Concat(otherErrors)),
+                value: _ => Failed<Func<T2, T3, T4, T5, T6, T7, T8, TResult>>(errors)),
+            value: f => value.Match(
+                errors: Failed<Func<T2, T3, T4, T5, T6, T7, T8, TResult>>,
+                value: t1 => Succeed((T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8) => f(t1, t2, t3, t4, t5, t6, t7, t8))));
+
     public static Validated<Func<T2, T3, T4, T5, T6, T7, TResult>> Apply<T1, T2, T3, T4, T5, T6, T7, TResult>(
         this Func<T1, T2, T3, T4, T5, T6, T7, TResult> map,
         Validated<T1> value) =>
