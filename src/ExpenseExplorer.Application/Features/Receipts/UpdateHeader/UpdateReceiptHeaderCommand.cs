2f0e9dc71f9e18d3db9566d0a047336319191924
frognar <sebastian.przyszlak@gmail.com>
2025-06-19 07:10:16 +0200

    Introduce `UpdateReceiptHeaderCommand` and factory for update receipt header operation

---
+++ src/ExpenseExplorer.Application/Features/Receipts/UpdateHeader/UpdateReceiptHeaderCommand.cs
@@ -0,0 +1,13 @@
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
+
+namespace ExpenseExplorer.Application.Features.Receipts.UpdateHeader;
+
+public sealed record UpdateReceiptHeaderCommand(ReceiptId ReceiptId, Store Store, PurchaseDate PurchaseDate)
+    : ICommand<UpdateReceiptHeaderResponse>;
+
+public static class UpdateReceiptHeaderCommandFactory
+{
+    public static Func<ReceiptId, Store, PurchaseDate, UpdateReceiptHeaderCommand> Create => (rId, s, p) =>
+        new UpdateReceiptHeaderCommand(rId, s, p);
+}
