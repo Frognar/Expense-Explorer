c9e220f8f4a8eb16aed9c61ceb461788dbdf4f88
frognar <sebastian.przyszlak@gmail.com>
2025-06-21 06:28:48 +0200

    Introduce `DeleteReceiptHeaderCommandHandler` for handling delete receipt header logic

---
+++ src/ExpenseExplorer.Application/Features/Receipts/DeleteHeader/DeleteReceiptHeaderCommandHandler.cs
@@ -0,0 +1,20 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using ExpenseExplorer.Application.Domain.Receipts;
+using ExpenseExplorer.Application.Receipts.ValueObjects;
+
+namespace ExpenseExplorer.Application.Features.Receipts.DeleteHeader;
+
+public sealed class DeleteReceiptHeaderCommandHandler(
+    IDeleteReceiptHeaderPersistence persistence)
+    : ICommandHandler<DeleteReceiptHeaderCommand, DeleteReceiptHeaderResponse>
+{
+    public async Task<Result<DeleteReceiptHeaderResponse>> HandleAsync(DeleteReceiptHeaderCommand command,
+        CancellationToken cancellationToken)
+    {
+        return await persistence.GetReceiptByIdAsync(command.ReceiptId, cancellationToken)
+            .BindAsync(r => r.Delete())
+            .BindAsync(async r => await persistence.SaveReceiptAsync(r, cancellationToken))
+            .MapAsync(_ => new DeleteReceiptHeaderResponse());
+    }
+}
