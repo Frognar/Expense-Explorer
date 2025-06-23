f14d810fef07e7d4cac89867972d5cf4da8527d5
frognar <sebastian.przyszlak@gmail.com>
2025-06-23 06:58:48 +0200

    Introduce `GetReceiptSummariesQueryHandler` to handle receipt summaries retrieval logic

---
+++ src/ExpenseExplorer.Application/Features/Receipts/GetReceipts/GetReceiptSummariesQueryHandler.cs
@@ -0,0 +1,21 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+
+namespace ExpenseExplorer.Application.Features.Receipts.GetReceipts;
+
+public sealed class GetReceiptSummariesQueryHandler(
+    IGetReceiptSummariesPersistence persistence)
+    : IQueryHandler<GetReceiptSummariesQuery, GetReceiptSummariesResponse>
+{
+    public async Task<Result<GetReceiptSummariesResponse>> HandleAsync(
+        GetReceiptSummariesQuery query,
+        CancellationToken cancellationToken)
+    {
+        return await
+            from receipts in persistence
+                .GetReceiptsAsync(query.PageSize, query.Skip, query.Order, query.Filters, cancellationToken)
+            from total in persistence
+                .GetTotalCostAsync(query.Filters, cancellationToken)
+            select new GetReceiptSummariesResponse(receipts, total);
+    }
+}
