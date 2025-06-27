d86692b1473bd15d2fd3b5b950a1f143206789ce
frognar <sebastian.przyszlak@gmail.com>
2025-06-27 07:34:00 +0200

    Add `GetStoresCommandHandler` to handle store query logic

---
+++ src/ExpenseExplorer.Application/Features/Stores/GetStores/GetStoresCommandHandler.cs
@@ -0,0 +1,15 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using static ExpenseExplorer.Application.Helpers.Splitter;
+
+namespace ExpenseExplorer.Application.Features.Stores.GetStores;
+
+public sealed class GetStoresCommandHandler(IGetStoresPersistence persistence)
+    : IQueryHandler<GetStoresRequest, GetStoresResponse>
+{
+    public async Task<Result<GetStoresResponse>> HandleAsync(
+        GetStoresRequest query,
+        CancellationToken cancellationToken) =>
+        await persistence.GetStoresAsync(SplitToUpper(query.SearchTerm), cancellationToken)
+            .MapAsync(s => new GetStoresResponse(s));
+}
