2e2fce3776b181851f4aa5df0e07845ba4c99f0f
frognar <sebastian.przyszlak@gmail.com>
2025-06-27 07:34:42 +0200

    Add `GetItemsCommandHandler` to handle items query logic

---
+++ src/ExpenseExplorer.Application/Features/Items/GetItems/GetItemsCommandHandler.cs
@@ -0,0 +1,15 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+using static ExpenseExplorer.Application.Helpers.Splitter;
+
+namespace ExpenseExplorer.Application.Features.Items.GetItems;
+
+public sealed class GetItemsCommandHandler(IGetItemsPersistence persistence)
+    : IQueryHandler<GetItemsRequest, GetItemsResponse>
+{
+    public async Task<Result<GetItemsResponse>> HandleAsync(
+        GetItemsRequest query,
+        CancellationToken cancellationToken) =>
+        await persistence.GetItemsAsync(SplitToUpper(query.SearchTerm), cancellationToken)
+            .MapAsync(s => new GetItemsResponse(s));
+}
