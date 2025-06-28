b14b4a319bd9e5e5019ae0941b8520a72757106b
frognar <sebastian.przyszlak@gmail.com>
2025-06-28 08:01:03 +0200

    Add `ReceiptItemOrder` record for defining receipt item sorting options

---
+++ src/ExpenseExplorer.Application/Features/ReceiptItems/GetReceiptItems/ReceiptItemOrder.cs
@@ -0,0 +1,14 @@
+namespace ExpenseExplorer.Application.Features.ReceiptItems.GetReceiptItems;
+
+public sealed record ReceiptItemOrder(string OrderBy, bool Descending)
+{
+    public static ReceiptItemOrder Id => new("Id", false);
+    public static ReceiptItemOrder Store => new("Store", false);
+    public static ReceiptItemOrder PurchaseDate => new("PurchaseDate", false);
+    public static ReceiptItemOrder Item => new("Item", false);
+    public static ReceiptItemOrder Category => new("Category", false);
+    public static ReceiptItemOrder UnitPrice => new("UnitPrice", false);
+    public static ReceiptItemOrder Quantity => new("Quantity", false);
+    public static ReceiptItemOrder Discount => new("Discount", false);
+    public static ReceiptItemOrder Total => new("Total", false);
+}
