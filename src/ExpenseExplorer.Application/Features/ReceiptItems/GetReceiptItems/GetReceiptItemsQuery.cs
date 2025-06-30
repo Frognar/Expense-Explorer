a838a63c7b688c456a1adb4a10e4633bb921a617
frognar <sebastian.przyszlak@gmail.com>
2025-06-30 05:57:13 +0200

    Enhance `GetReceiptItemsQuery` with default instance, filtering, sorting, and pagination utilities.

--- src/ExpenseExplorer.Application/Features/ReceiptItems/GetReceiptItems/GetReceiptItemsQuery.cs
+++ src/ExpenseExplorer.Application/Features/ReceiptItems/GetReceiptItems/GetReceiptItemsQuery.cs
@@ -7,4 +7,20 @@ public record GetReceiptItemsQuery(
     int Skip,
     ReceiptItemOrder Order,
     IEnumerable<ReceiptItemFilter> Filters)
-    : IQuery<GetReceiptItemsResponse>;
+    : IQuery<GetReceiptItemsResponse>
+{
+    public static GetReceiptItemsQuery Default =>
+        new(20, 0, ReceiptItemOrder.Id with { Descending = true }, []);
+
+    public GetReceiptItemsQuery Where(ReceiptItemFilter filter) =>
+        this with { Filters = Filters.Append(filter) };
+
+    public GetReceiptItemsQuery OrderBy(ReceiptItemOrder order) =>
+        this with { Order = order };
+
+    public GetReceiptItemsQuery OrderByDescending(ReceiptItemOrder order) =>
+        this with { Order = order with { Descending = true } };
+
+    public GetReceiptItemsQuery GetPage(int pageSize, int skip) =>
+        this with { PageSize = pageSize, Skip = skip };
+}
