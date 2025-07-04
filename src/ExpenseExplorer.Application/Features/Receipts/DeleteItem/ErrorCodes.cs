d769de77619471b14a134ef70c2ca719cde31fea
frognar <sebastian.przyszlak@gmail.com>
2025-07-04 09:35:10 +0200

    Create `ErrorCodes` for `DeleteItem` feature and define constants for validation errors.

---
+++ src/ExpenseExplorer.Application/Features/Receipts/DeleteItem/ErrorCodes.cs
@@ -0,0 +1,7 @@
+namespace ExpenseExplorer.Application.Features.Receipts.DeleteItem;
+
+internal static class ErrorCodes
+{
+    public const string InvalidReceiptItemId = "INVALID_RECEIPT_ITEM_ID";
+    public const string InvalidReceiptId = "INVALID_RECEIPT_ID";
+}
