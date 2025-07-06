327e4fd63d04fdeb9e01622abffd7644c56c98ba
frognar <sebastian.przyszlak@gmail.com>
2025-07-06 06:34:39 +0200

    Add `GetCategoryReportQueryHandler` for handling category report queries

---
+++ src/ExpenseExplorer.Application/Features/Reports/GetCategoryReport/GetCategoryReportQueryHandler.cs
@@ -0,0 +1,16 @@
+using DotResult;
+using ExpenseExplorer.Application.Abstractions.Messaging;
+
+namespace ExpenseExplorer.Application.Features.Reports.GetCategoryReport;
+
+public sealed class GetCategoryReportQueryHandler(IGetCategoryReportPersistence persistence)
+    : IQueryHandler<GetCategoryReportQuery, GetCategoryReportResponse>
+{
+    public async Task<Result<GetCategoryReportResponse>> HandleAsync(
+        GetCategoryReportQuery query,
+        CancellationToken cancellationToken)
+    {
+        return await persistence.GetCategoryReportAsync(query.From, query.To, cancellationToken)
+            .MapAsync(expenses => new GetCategoryReportResponse(expenses));
+    }
+}
